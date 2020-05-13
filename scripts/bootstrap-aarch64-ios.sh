ac14773e7bd55962b83eddc88fda08c2d466ea58
Syoyo Fujita <syoyo@lighttransport.com>
2020-05-13 15:00:48 +0900

    Add ios toolchain.

--- scripts/bootstrap-aarch64-ios.sh
+++ scripts/bootstrap-aarch64-ios.sh
@@ -12,5 +12,5 @@ $CMAKE_BIN \
 -DCMAKE_CXX_COMPILER=clang++ \
 -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk \
 -DCMAKE_OSX_ARCHITECTURES=arm64e \
--DCMAKE_TOOLCHAIN_FILE=../common/cmake/toolchains/ios.toolchain.cmake \
--Bbuild-aarch64-cross -H.
+-DCMAKE_TOOLCHAIN_FILE=./cmake/ios.toolchain.cmake \
+-Bbuild-aarch64-cross .
