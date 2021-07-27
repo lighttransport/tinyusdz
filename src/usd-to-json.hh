e857a7dfaba967a68308eaf6c923c1eeb6f66792
Syoyo Fujita <syoyo@lighttransport.com>
2021-07-28 03:34:03 +0900

    Add nlohmann json.hpp

---
+++ src/usd-to-json.hh
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// Experimental USD to JSON converter
+
+#include "nonstd/expected.hpp"
+
+namespace tinyusd {
+
+nonstd::expected<std::string, std::string> ToJSON();
+
+
+} // namespace tinyusd
