17554b1f646311b3e44a90b0c1caba888781deb6
Syoyo Fujita <syoyo@lighttransport.com>
2021-07-28 18:11:16 +0900

    Suppress clang warnings.

--- src/usd-to-json.cc
+++ src/usd-to-json.cc
@@ -3,12 +3,23 @@
 
 #include "tinyusdz.hh"
 
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Weverything"
+#endif
+
 // nlohmann json
 #include "nlohmann/json.hpp"
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
 
 using namespace nlohmann;
 
 namespace tinyusd {
+
+namespace {
 
 json ToJSON(tinyusdz::GeomMesh& mesh) {
   json j;
@@ -25,7 +36,8 @@ json ToJSON(tinyusdz::GeomMesh& mesh) {
     j["faceVertexIndices"] = mesh.faceVertexIndices;
   }
 
-  { auto normals = mesh.normals.buffer.GetAsVec3fArray();
+  {
+    auto normals = mesh.normals.buffer.GetAsVec3fArray();
     if (normals.size()) {
       j["normals"] = normals;
     }
@@ -35,12 +47,35 @@ json ToJSON(tinyusdz::GeomMesh& mesh) {
 
   j["purpose"] = mesh.purpose;
 
-	return j;
+  return j;
 }
 
+json ToJSON(tinyusdz::GeomBasisCurves& curves) {
+  json j;
+
+  if (curves.points.size()) {
+    j["points"] = curves.points;
+  }
+
+  j["purpose"] = curves.purpose;
+
+  return j;
+}
+
+}  // namespace
+
 nonstd::expected<std::string, std::string> ToJSON() {
+  tinyusdz::GeomMesh mesh;
+  json jmesh = ToJSON(mesh);
+
+  (void)jmesh;
+
+  tinyusdz::GeomBasisCurves curves;
+  json jcurves = ToJSON(curves);
+
+  (void)jcurves;
+
   return nonstd::make_unexpected("TODO");
 }
 
-
-} // namespace tinyusd
+}  // namespace tinyusd
--- src/usd-to-json.hh
+++ src/usd-to-json.hh
@@ -1,7 +1,16 @@
 // SPDX-License-Identifier: MIT
 // Experimental USD to JSON converter
 
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Weverything"
+#endif
+
 #include "nonstd/expected.hpp"
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
 
 namespace tinyusd {
 
