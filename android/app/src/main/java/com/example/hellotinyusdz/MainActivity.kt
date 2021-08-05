b93e8a15ff760f4c1e2858893e6781037975678f
Syoyo Fujita <syoyo@lighttransport.com>
2021-08-06 02:45:13 +0900

    Support touch event.

--- android/app/src/main/java/com/example/hellotinyusdz/MainActivity.kt
+++ android/app/src/main/java/com/example/hellotinyusdz/MainActivity.kt
@@ -22,17 +22,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
+import android.view.GestureDetector
 import android.widget.ImageView
 import android.widget.Toast
 import android.graphics.Bitmap
+import android.util.Log
+
+import androidx.core.view.MotionEventCompat
+
 import android.graphics.Color
 import android.graphics.BitmapFactory
 import kotlinx.android.synthetic.main.activity_main.sample_text
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), GestureDetector.OnGestureListener {
 
-    val render_width = 512;
-    val render_height = 512;
+    val render_width = 512
+    val render_height = 512
+    var lastTouchTime: Long = -1
+    var motionX = -1.0f
+    var motionY = -1.0f
 
     fun updateRender() {
         renderImage(render_width, render_height);
@@ -51,6 +59,106 @@ class MainActivity : AppCompatActivity() {
         img.setImageBitmap(b)
     }
 
+
+    override fun onLongPress(event: MotionEvent) {
+        Log.d("tinyusdz", "onLongPress: $event")
+    }
+
+    override fun onScroll(
+        event1: MotionEvent,
+        event2: MotionEvent,
+        distanceX: Float,
+        distanceY: Float
+    ): Boolean {
+        Log.d("tinyusdz", "onScroll: $event1 $event2")
+        return true
+    }
+
+    override fun onShowPress(event: MotionEvent) {
+
+        Log.d("tinyusdz", "onShowPress: $event")
+    }
+
+    override fun onSingleTapUp(event: MotionEvent): Boolean {
+        Log.d("tinyusdz", "onSingleTapUp: $event")
+        return true
+    }
+
+    override fun onDown(event: MotionEvent): Boolean {
+        Log.d("tinyusdz", "onDown: $event")
+        return true
+    }
+
+    override fun onFling(
+        event1: MotionEvent,
+        event2: MotionEvent,
+        velocityX: Float,
+        velocityY: Float
+    ): Boolean {
+        Log.d("tinyusdz", "onFling: $event1 $event2")
+        return true
+    }
+
+    override fun onTouchEvent(event: MotionEvent) : Boolean {
+        val action: Int = MotionEventCompat.getActionMasked(event)
+
+        return when (action) {
+            MotionEvent.ACTION_DOWN -> {
+                Log.d("tinyusdz", "Action was DOWN")
+                motionX = event.getX()
+                motionY = event.getY()
+
+                true
+            }
+            MotionEvent.ACTION_MOVE -> {
+                var x = event.getX()
+                var y = event.getY()
+
+                if ((lastTouchTime < 0) || (event.eventTime < lastTouchTime)) {
+                    motionX = x
+                    motionY = y
+                }
+
+                var eventDuration = event.eventTime - event.downTime;
+                lastTouchTime = eventDuration
+
+
+                var dx = x - motionX
+                var dy = y - motionY
+
+                Log.d("tinyusdz", "Action was MOVE. duration " + eventDuration + ", dx " + dx + ", dy = " + dy)
+
+                touchMove(dx, dy)
+
+                updateRender()
+
+                motionX = x
+                motionY = y
+
+                true
+            }
+            MotionEvent.ACTION_UP -> {
+                Log.d("tinyusdz", "Action was UP")
+
+                motionX = event.getX()
+                motionY = event.getY()
+
+                true
+            }
+            MotionEvent.ACTION_CANCEL -> {
+                Log.d("tinyusdz", "Action was CANCEL")
+                true
+            }
+            MotionEvent.ACTION_OUTSIDE -> {
+                Log.d("tinyusdz", "Movement occurred outside bounds of current screen element")
+                true
+            }
+            else -> {
+                Log.d("tinyusdz", "Another action")
+                super.onTouchEvent(event)
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -96,6 +204,7 @@ class MainActivity : AppCompatActivity() {
     private external fun initScene(mgr: AssetManager, filename: String) : Int
     private external fun renderImage(width: Int, height: Int) : Int
     private external fun grabImage(img: IntArray, width: Int, height: Int) : Int
+    private external fun touchMove(dx: Float, dy: Float) : Int
 
     companion object {
         // Used to load native code calling oboe on app startup.
