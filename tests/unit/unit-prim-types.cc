0d6ddcd8d3870c639dd4a5de66972d5178b082c6
Syoyo Fujita <syoyo@lighttransport.com>
2021-08-14 22:21:54 +0900

    Start to add unit tester. Support array(vector) type up to 3D.

---
+++ tests/unit/unit-prim-types.cc
@@ -0,0 +1,34 @@
+#define TEST_NO_MAIN
+#include "acutest.h"
+
+#include "unit-prim-types.h"
+#include "prim-types.hh"
+
+using namespace tinyusdz;
+
+void prim_type_test(void) {
+  {
+    PrimValue<float> pf;
+    TEST_CHECK(pf.array_dim() == 0);
+    TEST_CHECK(pf.type_name() == "float");
+  }
+
+  {
+    PrimValue<std::vector<float>> v;
+    TEST_CHECK(v.array_dim() == 1);
+    TEST_CHECK(v.type_name() == "float[]");
+  }
+
+  {
+    PrimValue<std::vector<std::vector<float>>> v;
+    TEST_CHECK(v.array_dim() == 2);
+    TEST_CHECK(v.type_name() == "float[][]");
+  }
+
+  {
+    PrimValue<std::vector<std::vector<std::vector<float>>>> v;
+    TEST_CHECK(v.array_dim() == 3);
+    TEST_CHECK(v.type_name() == "float[][][]");
+  }
+
+}
---
+++ tests/unit/unit-prim-types.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void prim_type_test(void);
