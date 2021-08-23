ea61a6c759f61248dafc3c07ef6c712f0f19c6ce
Syoyo Fujita <syoyo@lighttransport.com>
2021-08-23 20:48:23 +0900

    gcc4.8 -> gcc4.9

--- scripts/bootstrap-gcc-4.9.sh
+++ scripts/bootstrap-gcc-4.9.sh
@@ -1,13 +1,13 @@
 curdir=`pwd`
 
-builddir=${curdir}/build-gcc4.8
+builddir=${curdir}/build-gcc4.9
 
 rm -rf ${builddir}
 mkdir ${builddir}
 
 cd ${builddir} && cmake \
-  -DCMAKE_C_COMPILER=gcc-4.8 \
-  -DCMAKE_CXX_COMPILER=g++-4.8 \
+  -DCMAKE_C_COMPILER=gcc-4.9 \
+  -DCMAKE_CXX_COMPILER=g++-4.9 \
   -DCMAKE_VERBOSE_MAKEFILE=1 \
   ..
 
