3cddd41ce3bd4d0cec93e4f396926afcd00737f0
Syoyo Fujita <syoyo@lighttransport.com>
2021-08-28 18:44:09 +0900

    Refactor USDAParser.

--- src/usda-parser.hh
+++ src/usda-parser.hh
@@ -1,16 +1,61 @@
 // SPDX-License-Identifier: MIT
 #pragma once
 
-#include <string>
+#include "tinyusdz.hh"
 
 namespace tinyusdz {
 
 namespace usda {
 
+enum LoadState {
+  LOAD_STATE_TOPLEVEL,   // toplevel .usda input
+  LOAD_STATE_SUBLAYER,   // .usda is read by 'subLayers'
+  LOAD_STATE_REFERENCE,  // .usda is read by `references`
+  LOAD_STATE_PAYLOAD,    // .usda is read by `payload`
+};
+
+
 ///
 /// Test if input file is USDA format.
 ///
-bool IsUSDA(const std::string &filename);
+bool IsUSDA(const std::string &filename, size_t max_filesize = 0);
+
+class USDAParser {
+ public:
+  struct ParseState {
+    int64_t loc{-1};  // byte location in StreamReder
+  };
+
+  USDAParser() = delete;
+  USDAParser(tinyusdz::StreamReader *sr);
+
+  USDAParser(const USDAParser &rhs) = delete;
+  USDAParser(USDAParser &&rhs) = delete;
+
+  ~USDAParser();
+
+
+  ///
+  /// Base filesystem directory to search asset files.
+  ///
+  void SetBaseDir(const std::string &base_dir);
+
+  ///
+  /// Check if header data is USDA
+  ///
+  bool CheckHeader();
+
+  ///
+  /// Parser entry point
+  ///
+  bool Parse(LoadState state = LOAD_STATE_TOPLEVEL);
+
+  std::string GetError();
+
+ private:
+  class Impl;
+  Impl *_impl;
+};
 
 } // namespace usda
 
