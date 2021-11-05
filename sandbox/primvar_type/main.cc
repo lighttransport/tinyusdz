e41cbee4d74446ac86a16f503b9e94aa2294cd1b
Syoyo Fujita <syoyo@lighttransport.com>
2021-11-06 00:47:46 +0900

    primvar type experiment.

---
+++ sandbox/primvar_type/main.cc
@@ -0,0 +1,90 @@
+#include <iostream>
+
+//
+// Simple variant-lile type
+//
+
+template<class dtype>
+struct TypeTrait;
+
+template<>
+struct TypeTrait<float> {
+  static constexpr auto type_name = "float";
+};
+
+template<>
+struct TypeTrait<double> {
+  static constexpr auto type_name = "double";
+};
+
+struct Value
+{
+  std::string type_name;
+
+  union {
+    float _float;
+    double _double;
+    int _int;
+  } u;
+
+  template<class T>
+  Value &operator=(const T &t);
+
+  template<class T>
+  T *get_if();
+  
+};
+
+
+#define DEFINE_ASSIGN(__type, __varname) \
+template<> \
+Value &Value::operator=(const __type &v) { \
+  type_name = TypeTrait<__type>::type_name; \
+  u.__varname = v;  \
+ \
+  return (*this); \
+}
+
+DEFINE_ASSIGN(double, _double);
+DEFINE_ASSIGN(float, _float);
+
+#undef DEFINE_ASSIGN
+
+template<>
+double *Value::get_if<double>() {
+  if (type_name == TypeTrait<double>::type_name) {
+    return &u._double;
+  }
+
+  return nullptr;
+}
+
+std::ostream &operator<<(std::ostream &os, const Value &v)
+{
+  
+  os << "(type: " << v.type_name << ") ";
+  if (v.type_name == "float") {
+    os << v.u._float;
+  } else if (v.type_name == "double") {
+    os << v.u._double;
+  } else {
+    os << "TODO";
+  }
+
+  return os;
+}
+
+int main(int argc, char **argv)
+{
+  Value v;
+
+  v = 1.3f;
+  v = 1.3;
+
+  std::cout << v << "\n";
+
+  if (v.get_if<double>()) {
+    std::cout << "double!" << "\n";
+  }
+
+}
