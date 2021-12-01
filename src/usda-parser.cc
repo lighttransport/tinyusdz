eff2ab88eb60e51d1c42223ef15f543e1b0c8e41
Syoyo Fujita <syoyo@lighttransport.com>
2021-12-01 16:51:07 +0900

    Remove unused code.

--- src/usda-parser.cc
+++ src/usda-parser.cc
@@ -327,13 +327,17 @@ class Variable {
 
     } else if (v.IsTimeSamples()) {
       std::string ts_type = "none";
+      // FIXME
+#if 0
       auto ts_struct = v.as_timesamples();
+
       for (const TimeSampleType &item : ts_struct->values) {
         auto tname = primvar::type_name(item);
         if (tname != "none") {
           return tname;
         }
       }
+#endif
 
       // ??? TimeSamples data contains all `None` values
       return ts_type;
@@ -7202,7 +7206,8 @@ bool USDAParser::Impl::ReconstructGeomMesh(
           LOG_INFO("points");
           const Property &prop = gprim.props.at("points");
           if (auto pattr = nonstd::get_if<PrimAttrib>(&prop)) {
-            LOG_INFO("pattr:" + primvar::type_name(pattr->var));
+            // PrimVar
+            LOG_INFO("pattr:" + tinyusdz::get_type_name(pattr->var));
             if (auto p = primvar::as_vector<Vec3f>(&pattr->var)) {
               LOG_INFO("points. sz = " + std::to_string(p->size()));
               mesh->points = (*p);
@@ -8128,63 +8133,3 @@ std::string USDAParser::GetError() { return _impl->GetError(); }
 
 }  // namespace tinyusdz
 
-#if defined(USDA_MAIN)
-
-int main(int argc, char **argv) {
-  if (argc < 2) {
-    std::cout << "Need input.usda\n";
-    exit(-1);
-  }
-
-  std::string filename = argv[1];
-
-  std::string base_dir;
-  base_dir = tinyusdz::io::GetBaseDir(filename);
-
-  std::vector<uint8_t> data;
-  {
-    // TODO(syoyo): Support UTF-8 filename
-    std::ifstream ifs(filename.c_str(), std::ifstream::binary);
-    if (!ifs) {
-      std::cerr << "Failed to open file: " << filename << "\n";
-      return -1;
-    }
-
-    // TODO(syoyo): Use mmap
-    ifs.seekg(0, ifs.end);
-    size_t sz = static_cast<size_t>(ifs.tellg());
-    if (int64_t(sz) < 0) {
-      // Looks reading directory, not a file.
-      std::cerr << "Looks like filename is a directory : \"" << filename
-                << "\"\n";
-      return -1;
-    }
-
-    data.resize(sz);
-
-    ifs.seekg(0, ifs.beg);
-    ifs.read(reinterpret_cast<char *>(&data.at(0)),
-             static_cast<std::streamsize>(sz));
-  }
-
-  tinyusdz::StreamReader sr(data.data(), data.size(), /* swap endian */ false);
-  tinyusdz::usda::USDAParser parser(&sr);
-
-  std::cout << "Basedir = " << base_dir << "\n";
-  parser.SetBaseDir(base_dir);
-
-  {
-    bool ret = parser.Parse();
-
-    if (!ret) {
-      std::cerr << "Failed to parse .usda: \n";
-      std::cerr << parser.GetError() << "\n";
-      return -1;
-    } else {
-      std::cout << "ok\n";
-    }
-  }
-
-  return 0;
-}
-#endif
