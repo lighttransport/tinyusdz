fd1439f70a9e2857f0942cd9ff58bec1cc794f52
Syoyo Fujita <syoyo@lighttransport.com>
2022-07-11 19:31:38 +0900

    Rewriting nonstd::variant to tinyusdz::variant(W.I.P.)

--- src/simple-variant.hh
+++ src/simple-variant.hh
@@ -26,6 +26,11 @@ For more information, please refer to <http://unlicense.org/>
 #include <iostream>
 #include <string>
 
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Weverything"
+#endif
+
 namespace tinyusdz {
 
 // Equivalent to std::aligned_storage
@@ -253,5 +258,12 @@ public:
 		helper_t::destroy(variant_id, &data);
 	}
 };
+
+struct monostate {
+};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
 
 } // namespace tinyusdz
