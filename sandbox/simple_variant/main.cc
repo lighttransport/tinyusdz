3cd99537093f7bb47a944fc54e50a93dfb507dca
Syoyo Fujita <syoyo@lighttransport.com>
2022-07-11 22:38:20 +0900

    Refactor simple-variant.hh

--- sandbox/simple_variant/main.cc
+++ sandbox/simple_variant/main.cc
@@ -9,16 +9,18 @@ int main(int argc, char **argv) {
   myvar a;
   a.set<bool>(true);
 
+  a = 1.4f;
+
   myvar b;
   
   b = a;
 
   a.set<float>(1.3f);
 
-  std::cout << "val = " << a.get<float>() << "\n";
+  std::cout << "a val = " << a.get<float>() << "\n";
   
-  if (auto v = a.get_if<float>()) {
-    std::cout << "val = " << (*v) << "\n";
+  if (auto v = b.get_if<float>()) {
+    std::cout << "b val = " << (*v) << "\n";
   }
 
   return 0;
