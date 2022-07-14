eebec1d6b7d897321034da567cf447c0a5f55c17
Syoyo Fujita <syoyo@lighttransport.com>
2022-07-14 17:03:17 +0900

    Disable std::mutex on WASI target

--- sandbox/wasi/main.cc
+++ sandbox/wasi/main.cc
@@ -1,10 +1,12 @@
 #include <cstdio>
 #include <iostream>
 
+#include "tinyusdz.hh"
+
 int main(int argc, char **argv)
 {
   std::cout << "bora\n";
-  std::cout << "sz = " << sizeof(size_t) + "\n";
+  printf("sz = %d\n", sizeof(size_t));
 
   return 0;
 }
