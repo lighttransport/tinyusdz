2541467e46488600e1d36dc3a72220c364bb0460
Syoyo Fujita <syoyo@lighttransport.com>
2022-07-21 19:13:53 +0900

    Add benchmark(to measure the performance of C++ data structure).

---
+++ benchmarks/benchmark-main.cc
@@ -0,0 +1,54 @@
+#include <unistd.h>
+#include "ubench.h"
+
+#include "value-type.hh"
+
+UBENCH(perf, vector_double_push_back_10M)
+{
+  std::vector<double> v;
+  constexpr size_t niter = 10 * 10000;
+  for (size_t i = 0; i < niter; i++) {
+    v.push_back(double(i));
+  } 
+}
+
+UBENCH(perf, any_value_double_10M)
+{
+  constexpr size_t niter = 10 * 10000;
+  for (size_t i = 0; i < niter; i++) {
+    tinyusdz::value::any_value a;
+    a = double(i);
+  } 
+}
+
+#if 0
+UBENCH(perf, any_value_100M)
+{
+  constexpr size_t niter = 100 * 10000;
+  for (size_t i = 0; i < niter; i++) {
+    tinyusdz::value::any_value a;
+    a = i;
+  } 
+}
+#endif
+
+UBENCH(perf, timesamples_double_10M)
+{
+  constexpr size_t ns = 10 * 10000;
+
+  tinyusdz::value::TimeSamples ts;
+
+  for (size_t i = 0; i < ns; i++) {
+    ts.times.push_back(double(i));
+    ts.values.push_back(double(i));
+  } 
+}
+
+//int main(int argc, char **argv)
+//{
+//  benchmark_any_type();
+//
+//  return 0;
+//}
+
+UBENCH_MAIN();
