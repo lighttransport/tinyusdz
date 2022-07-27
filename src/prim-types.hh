779c20cd8815bb82f1af6cdb8dd75473037ce04a
Syoyo Fujita <syoyo@lighttransport.com>
2022-07-27 22:27:49 +0900

    Refactor a bit in USDA reader.

--- src/prim-types.hh
+++ src/prim-types.hh
@@ -1415,11 +1415,19 @@ struct LuxSphereLight {
   //
   // Predefined attribs.
   //
-  // TODO: Support texture?
-  value::color3f color{};
-  float intensity{10.0f};
-  float radius{0.1f};
-  float specular{1.0f};
+
+  // Light API
+  value::color3f color{1.0f, 1.0f, 1.0f}; // inputs.color Light energy in linear color space.
+  float colorTemperature{6500.0f};  // inputs:colorTemperature 
+  float diffuse{1.0f}; // inputs:diffuse diffuse multiplier
+  bool enableColorTemperature{false}; // inputs:enableColorTemperature
+  float exposure{0.0f}; // inputs:exposure EV
+  float intensity{1.0f}; // inputs:intensity
+  bool normalize{false}; // inputs:normalize normalize power by the surface area of the light.
+  float specular{1.0f}; // inputs:specular specular multiplier
+  // rel light:filters
+
+  float radius{0.5f}; // inputs:radius
 
   //
   // Properties
@@ -1438,13 +1446,36 @@ struct LuxDomeLight {
   std::string name;
   int64_t parent_id{-1};  // Index to xform node
 
+  enum class TextureFormat {
+    Automatic, // "automatic"
+    Latlong, // "latlong"
+    MirroredBall, // "mirroredBall"
+    Angular // "angular"
+  };
+
   //
   // Predefined attribs.
   //
-  // TODO: Support texture?
-  value::color3f color{};
-  float intensity{10.0f};
+  // TODO: Support texture
+
+  // Light API
+  value::color3f color{1.0f, 1.0f, 1.0f}; // inputs.color Light energy in linear color space.
+  float colorTemperature{6500.0f};  // inputs:colorTemperature 
+  float diffuse{1.0f}; // inputs:diffuse diffuse multiplier
+  bool enableColorTemperature{false}; // inputs:enableColorTemperature
+  float exposure{0.0f}; // inputs:exposure EV
+  float intensity{1.0f}; // inputs:intensity
+  bool normalize{false}; // inputs:normalize normalize power by the surface area of the light.
+  float specular{1.0f}; // inputs:specular specular multiplier
+  // rel light:filters
+
+
+  // DomeLight specific
   float guideRadius{1.0e5f};
+  // asset inputs:texture:file
+  TextureFormat textureFormat{TextureFormat::Automatic}; // token inputs:texture:format
+  // rel portals
+  // rel proxyPrim
 
   //
   // Properties
@@ -1452,6 +1483,7 @@ struct LuxDomeLight {
   AnimatableExtent extent;  // bounding extent(in local coord?).
   AnimatableVisibility visibility{Visibility::Inherited};
   Purpose purpose{Purpose::Default};
+  std::vector<value::token> xformOpOrder;
 
   // List of Primitive attributes(primvars)
   // NOTE: `primvar:widths` may exist(in that ase, please ignore `widths`
@@ -1499,9 +1531,10 @@ struct SkelRoot {
   Purpose purpose{Purpose::Default};
   AnimatableVisibility visibility{Visibility::Inherited};
 
-  // TODO
-  // std::vector<std::string> xformOpOrder;
+  // NOTE: SkelRoot itself does not have dedicated attributes in the schema.
+
   // ref proxyPrim
+  std::vector<value::token> xformOpOrder;
 
   int64_t skeleton_id{-1};  // index to scene.skeletons
   // Skeleton skeleton;
