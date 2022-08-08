590aae9ec0c637cf9e5b6607ed9549da58f57c21
Syoyo Fujita <syoyo@lighttransport.com>
2022-08-08 22:16:41 +0900

    Update python related files.

--- python/README.md
+++ python/README.md
@@ -1,5 +1,27 @@
-This folder is used for packaging Python binding of TinyUSDZ
+# Python binding of TinyUSDZ
 
-Core part is deletegated to native module(tinyusd)
+Currently in testing stage. Does not work well.
+
+Core part is deletegated to native module(tinyusd.so)
 
 W.I.P.
+
+## Supported platform
+
+* [ ] Linux
+  * [x] x86-64
+  * [ ] aarch64
+* [ ] Windows
+* [ ] macOS
+
+## Install through PyPI
+
+```
+$ python -m pip install tinyusdz
+```
+
+## License
+
+Currently MIT license, but soon move to Apache 2.0 license.
+
+EoL.
--- setup.py
+++ setup.py
@@ -1,72 +1,16 @@
-# based on https://stackoverflow.com/questions/42585210/extending-setuptools-extension-to-use-cmake-in-setup-py
 import os
 import pathlib
 
+# skbuild = python setuptools with Cmake support
 from skbuild import setup
-#from setuptools.command.build_ext import build_ext as build_ext_orig
-
-
-#class CMakeExtension(Extension):
-#
-#    def __init__(self, name):
-#        # don't invoke the original build_ext for this special extension
-#        super().__init__(name, sources=[])
-#
-#
-#class build_ext(build_ext_orig):
-#
-#    def run(self):
-#        for ext in self.extensions:
-#            self.build_cmake(ext)
-#        super().run()
-#
-#    def build_cmake(self, ext):
-#        cwd = pathlib.Path().absolute()
-#        print("cwd = ", cwd)
-#
-#        # these dirs will be created in build_py, so if you don't have
-#        # any python sources to bundle, the dirs will be missing
-#        build_temp = pathlib.Path(self.build_temp)
-#        build_temp.mkdir(parents=True, exist_ok=True)
-#        extdir = pathlib.Path(self.get_ext_fullpath(ext.name))
-#        extdir.mkdir(parents=True, exist_ok=True)
-#
-#        config = 'RelWithDebInfo'
-#        cmake_args = [
-#            '-S{}'.format(os.path.join(cwd, "..")),
-#            '-B{}'.format(build_temp),
-#            '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + str(extdir.parent.absolute()),
-#            '-DCMAKE_BUILD_TYPE=' + config
-#        ]
-#
-#        # example of build args
-#        build_args = [
-#            '--config', config,
-#            '--', '-j4'
-#        ]
-#
-#        print("cmake_args = ", cmake_args)
-#
-#        #os.chdir(str(build_temp))
-#        self.spawn(['cmake'] + cmake_args)
-#        if not self.dry_run:
-#            self.spawn(['cmake', '--build', str(build_temp)] + build_args)
-#        # Troubleshooting: if fail on line above then delete all possible 
-#        # temporary CMake files including "CMakeCache.txt" in top level dir.
-#        os.chdir(str(cwd))
-
 
 setup(
     name='tinyusdz',
     version='0.8.0rc0',
     author='Light Transport Entertainment Inc.',
     author_email='lighttransport.eth@mail3.me',
-    packages_dir=['python'], # Use `python` folder 
-    cmake_args=['-DTINYUSDZ_WITH_PYTHON=1'],
-    long_description=open("./README.md", 'r').read(),
-    #ext_modules=[CMakeExtension('pytinyusdz')],
+    package_dir={'tinyusdz': 'python'}, # Use `python` folder 
+    cmake_args=['-DTINYUSDZ_WITH_PYTHON=1', '-DTINYUSDZ_BUILD_EXAMPLES=Off', '-DTINYUSDZ_BUILD_TESTS=Off', '-DTINYUSDZ_WITH_TOOL_USDA_PARSER=Off'],
+    long_description=open("./python/README.md", 'r').read(),
     license='MIT',
-    #cmdclass={
-    #    'build_ext': build_ext,
-    #}
 )
