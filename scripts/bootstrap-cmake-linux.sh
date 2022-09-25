1f0130903c5392b8825dbdba888369f052a1ce19
Syoyo Fujita <syoyo@lighttransport.com>
2022-09-26 01:10:28 +0900

    Add cmake toolchain file for linking with LLD.

--- scripts/bootstrap-cmake-linux.sh
+++ scripts/bootstrap-cmake-linux.sh
@@ -5,6 +5,8 @@ builddir=${curdir}/build
 rm -rf ${builddir}
 mkdir ${builddir}
 
+# with lld linker
+#  -DCMAKE_TOOLCHAIN_FILE=cmake/lld-linux.toolchain.cmake 
 
 cd ${builddir} && cmake \
   -DCMAKE_VERBOSE_MAKEFILE=1 \
