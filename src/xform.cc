2e41827787efb44699c88c0f4d950479ba8fc62d
Syoyo Fujita <syoyo@lighttransport.com>
2022-09-30 23:16:02 +0900

    Initial implementation of full xformOps evaluation(not tested though)

--- src/xform.cc
+++ src/xform.cc
@@ -19,13 +19,75 @@
 
 namespace tinyusdz {
 
+using matrix44d = linalg::aliases::double4x4;
+using double3x3 = linalg::aliases::double3x3;
+
 // linalg quat: (x, y, z, w)
+
+value::matrix3d to_matrix3x3(const value::quath &q)
+{
+  double3x3 m33 = linalg::qmat<double>({double(half_to_float(q.imag[0])), double(half_to_float(q.imag[1])), double(half_to_float(q.imag[2])), double(half_to_float(q.real))});
+
+  value::matrix3d m;
+  Identity(&m);
+
+  memcpy(m.m, &m33[0][0], sizeof(double) * 3 * 3);
+
+  return m;
+}
+
+value::matrix3d to_matrix3x3(const value::quatf &q)
+{
+  double3x3 m33 = linalg::qmat<double>({double(q.imag[0]), double(q.imag[1]), double(q.imag[2]), double(q.real)});
+
+  value::matrix3d m;
+  Identity(&m);
+
+  memcpy(m.m, &m33[0][0], sizeof(double) * 3 * 3);
+
+  return m;
+}
+
+value::matrix3d to_matrix3x3(const value::quatd &q)
+{
+  double3x3 m33 = linalg::qmat<double>({q.imag[0], q.imag[1], q.imag[2], q.real});
+
+  value::matrix3d m;
+  Identity(&m);
+
+  memcpy(m.m, &m33[0][0], sizeof(double) * 3 * 3);
+
+  return m;
+}
+
+value::matrix4d to_matrix(const value::matrix3d &m33, const value::double3 &tx)
+{
+
+  value::matrix4d m;
+  Identity(&m);
+
+  m.m[0][0] = m33.m[0][0];
+  m.m[0][1] = m33.m[0][1];
+  m.m[0][2] = m33.m[0][2];
+  m.m[1][0] = m33.m[1][0];
+  m.m[1][1] = m33.m[1][1];
+  m.m[1][2] = m33.m[1][2];
+  m.m[2][0] = m33.m[2][0];
+  m.m[2][1] = m33.m[2][1];
+  m.m[2][2] = m33.m[2][2];
+
+  m.m[3][0] = tx[0];
+  m.m[3][1] = tx[1];
+  m.m[3][2] = tx[2];
+
+  return m;
+}
+
 
 value::matrix4d to_matrix(const value::quath &q)
 {
-  using double3x3 = linalg::aliases::double3x3;
   //using double4 = linalg::aliases::double4;
-  
+
   double3x3 m33 = linalg::qmat<double>({double(half_to_float(q.imag[0])), double(half_to_float(q.imag[1])), double(half_to_float(q.imag[2])), double(half_to_float(q.real))});
 
   value::matrix4d m;
@@ -46,8 +108,6 @@ value::matrix4d to_matrix(const value::quath &q)
 
 value::matrix4d to_matrix(const value::quatf &q)
 {
-  using double3x3 = linalg::aliases::double3x3;
-  
   double3x3 m33 = linalg::qmat<double>({double(q.imag[0]), double(q.imag[1]), double(q.imag[2]), double(q.real)});
 
   value::matrix4d m;
@@ -68,8 +128,6 @@ value::matrix4d to_matrix(const value::quatf &q)
 
 value::matrix4d to_matrix(const value::quatd &q)
 {
-  using double3x3 = linalg::aliases::double3x3;
-  
   double3x3 m33 = linalg::qmat<double>({q.imag[0], q.imag[1], q.imag[2], q.real});
 
   value::matrix4d m;
@@ -88,6 +146,58 @@ value::matrix4d to_matrix(const value::quatd &q)
   return m;
 }
 
+value::matrix4d invert(const value::matrix4d &_m) {
 
+  matrix44d m;
+  // memory layout is same
+  memcpy(&m[0][0], _m.m, sizeof(double) * 4 * 4);
+
+  matrix44d inv_m = linalg::inverse(m);
+
+  value::matrix4d outm;
+
+  memcpy(outm.m, &inv_m[0][0], sizeof(double) * 4 * 4);
+
+  return outm;
+}
+
+double determinant(const value::matrix4d &_m) {
+
+  matrix44d m;
+  // memory layout is same
+  memcpy(&m[0][0], _m.m, sizeof(double) * 4 * 4);
+
+  double det = linalg::determinant(m);
+
+  return det;
+}
+
+bool invert(const value::matrix4d &_m, value::matrix4d &inv_m) {
+
+  double det = determinant(_m);
+
+  // 1e-9 comes from pxrUSD
+  // determinant should be positive(absolute), but take a fabs() just in case.
+  if (std::fabs(det) < 1e-9) {
+    return false;
+  }
+
+  inv_m = invert(_m);
+  return true;
+}
+
+bool invert3x3(const value::matrix3d &_m, value::matrix3d &inv_m) {
+
+  double det = determinant3x3(_m);
+
+  // 1e-9 comes from pxrUSD
+  // determinant should be positive(absolute), but take a fabs() just in case.
+  if (std::fabs(det) < 1e-9) {
+    return false;
+  }
+
+  inv_m = invert3x3(_m);
+  return true;
+}
 
 } // namespace tinyusdz
--- src/xform.hh
+++ src/xform.hh
@@ -11,5 +11,22 @@ value::matrix4d to_matrix(const value::quath &q);
 value::matrix4d to_matrix(const value::quatf &q);
 value::matrix4d to_matrix(const value::quatd &q);
 
+value::matrix4d to_matrix(const value::matrix3d &m, const value::double3 &tx);
+
+value::matrix3d to_matrix3x3(const value::quath &q);
+value::matrix3d to_matrix3x3(const value::quatf &q);
+value::matrix3d to_matrix3x3(const value::quatd &q);
+
+value::matrix4d invert(const value::matrix4d &m);
+double determinant(const value::matrix4d &m);
+
+value::matrix3d invert3x3(const value::matrix3d &m);
+double determinant3x3(const value::matrix3d &m);
+
+// Do singular matrix check.
+// Return true and set `inv_m` when input matrix `m` can be inverted
+bool invert(const value::matrix4d &m, value::matrix4d &inv_m);
+bool invert3x3(const value::matrix3d &m, value::matrix3d &inv_m);
+
 
 } // namespace tinyusdz
