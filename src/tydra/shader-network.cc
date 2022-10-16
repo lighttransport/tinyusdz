efa2773f8d977d55bb9722302f1b6e0d21a10654
Syoyo Fujita <syoyo@lighttransport.com>
2022-10-17 03:39:10 +0900

    [Tydra] Add shader network(nothing yet though). Remove unused `experimental/serialize`

---
+++ src/tydra/shader-network.cc
@@ -0,0 +1,16 @@
+#include "shader-network.hh"
+
+#include "usdShade.hh"
+#include "pprinter.hh"
+#include "prim-pprint.hh"
+#include "value-pprint.hh"
+
+namespace tinyusdz {
+namespace tydra {
+
+namespace {
+
+} // namespace local
+
+} // namespace tydra
+} // namespace tinyusdz
