00c98a79d31faf8e14eaed7845200b3cb0f615ba
Syoyo Fujita <syoyo@lighttransport.com>
2022-12-24 06:19:33 +0900

    Implement more data types for vnormalize, vcross, etc.

--- src/linear-algebra.hh
+++ src/linear-algebra.hh
@@ -13,18 +13,36 @@ value::quatd slerp(const value::quatd &a, const value::quatd &b, const double t)
 float vlength(const value::float3 &a);
 float vlength(const value::normal3f &a);
 float vlength(const value::vector3f &a);
+float vlength(const value::point3f &a);
 double vlength(const value::double3 &a);
+double vlength(const value::normal3d &a);
+double vlength(const value::vector3d &a);
+double vlength(const value::point3d &a);
 
 value::float3 vnormalize(const value::float3 &a);
 value::double3 vnormalize(const value::double3 &a);
 value::normal3f vnormalize(const value::normal3f &a);
+value::normal3d vnormalize(const value::normal3d &a);
 value::vector3f vnormalize(const value::vector3f &a);
+value::vector3d vnormalize(const value::vector3d &a);
+value::point3f vnormalize(const value::point3f &a);
+value::point3d vnormalize(const value::point3d &a);
 
 // Assume CCW(Counter ClockWise)
 value::float3 vcross(const value::float3 &a, const value::float3 &b);
 value::double3 vcross(const value::double3 &a, const value::double3 &b);
+value::normal3f vcross(const value::normal3f &a, const value::normal3f &b);
+value::normal3d vcross(const value::normal3d &a, const value::normal3d &b);
+value::vector3f vcross(const value::vector3f &a, const value::vector3f &b);
+value::vector3d vcross(const value::vector3d &a, const value::vector3d &b);
+value::point3f vcross(const value::point3f &a, const value::point3f &b);
+value::point3d vcross(const value::point3d &a, const value::point3d &b);
+
 value::float3 geometric_normal(const value::float3 &p0, const value::float3 &p1, const value::float3 &p2);
 value::double3 geometric_normal(const value::double3 &p0, const value::double3 &p1, const value::double3 &p2);
+value::point3f geometric_normal(const value::point3f &p0, const value::point3f &p1, const value::point3f &p2);
+value::point3d geometric_normal(const value::point3d &p0, const value::point3d &p1, const value::point3d &p2);
+
 
 
 inline float vdot(const value::float3 &a, const value::float3 &b)
@@ -33,6 +51,26 @@ inline float vdot(const value::float3 &a, const value::float3 &b)
 }
 
 inline double vdot(const value::double3 &a, const value::double3 &b)
+{
+  return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+}
+
+inline float vdot(const value::vector3f &a, const value::vector3f &b)
+{
+  return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+}
+
+inline double vdot(const value::vector3d &a, const value::vector3d &b)
+{
+  return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+}
+
+inline float vdot(const value::normal3f &a, const value::normal3f &b)
+{
+  return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+}
+
+inline double vdot(const value::normal3d &a, const value::normal3d &b)
 {
   return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
 }
