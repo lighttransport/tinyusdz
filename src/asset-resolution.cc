9c8ee1a28325df9b0003e058bebb96666f464786
Syoyo Fujita <syoyo@lighttransport.com>
2023-01-25 18:05:52 +0900

    [Tydra] print all UsdPreviewSurface parameters.

--- src/asset-resolution.cc
+++ src/asset-resolution.cc
@@ -1,16 +1,24 @@
 // SPDX-License-Identifier: Apache 2.0
 // Copyright 2022 - Present, Light Transport Entertainment, Inc.
+#include <iostream>
+
 #include "asset-resolution.hh"
 #include "io-util.hh"
+#include "common-macros.inc"
+#include "value-pprint.hh"
 
 namespace tinyusdz {
 
 bool AssetResolutionResolver::find(const std::string &assetPath) {
+  DCOUT("search_paths = " << _search_paths);
+  DCOUT("assetPath = " << assetPath);
   std::string fpath = io::FindFile(assetPath, _search_paths);
   return fpath.size();
 }
 
 std::string AssetResolutionResolver::resolve(const std::string &assetPath) {
+  DCOUT("search_paths = " << _search_paths);
+  DCOUT("assetPath = " << assetPath);
   return io::FindFile(assetPath, _search_paths);
 }
 
