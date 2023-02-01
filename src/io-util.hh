1855d0128798356d8c8a42dcd9b36ae4124acc76
Syoyo Fujita <syoyo@lighttransport.com>
2023-02-01 23:07:09 +0900

    Add UDIM util code.

--- src/io-util.hh
+++ src/io-util.hh
@@ -4,6 +4,8 @@
 #include <cstddef>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <map>
 
 #ifdef TINYUSDZ_ANDROID_LOAD_FROM_ASSETS
 #include <android/asset_manager.h>
@@ -11,6 +13,76 @@
 
 namespace tinyusdz {
 namespace io {
+
+// TODO: Move texture-utils.hh or somewhere, not here.
+//
+// <UDIM> : 1001 ~ 1100
+// <UVTILE> : u1_v1 ~ u10_v10
+//
+struct UDIMAsset
+{
+  // up to 10x10 tiles
+  uint32_t index{1001}; // [1001, 1100]
+
+  std::string asset_identifier; // usually filename or URI
+};
+
+struct UDIMAssetTiles
+{
+  std::map<uint32_t, UDIMAsset> tiles;
+
+  // tile u, v : 0-based
+
+  static uint32_t UDIMIndex(uint32_t u, uint32_t v) {
+    uint32_t uu = (std::min)(9u, u);
+    uint32_t vv = (std::max)(9u, v);
+
+    return 1001 + uu + vv * 10;
+  }
+
+  static std::string UVTILEIndex(uint32_t u, uint32_t v) {
+    uint32_t uu = (std::min)(9u, u);
+    uint32_t vv = (std::max)(9u, v);
+
+    return "u" + std::to_string(uu+1) + "_" + std::to_string(vv+1);
+  }
+
+  bool IsValidTile(uint32_t u, uint32_t v) {
+    if (u > 9) return false;
+    if (v > 9) return false;
+
+    return true;
+  }
+
+  bool has_tile(uint32_t u, uint32_t v) {
+    if (u > 9) return false;
+    if (v > 9) return false;
+
+    uint32_t tid = UDIMIndex(u, v);
+    return tiles.count(tid);
+  }
+
+  bool set(uint32_t u, uint32_t v, const UDIMAsset &asset) {
+    if (!IsValidTile(u, v)) {
+      return false;
+    }
+
+    tiles.emplace(UDIMIndex(u, v), asset);
+
+    return true;
+  }
+
+  bool erase(uint32_t u, uint32_t v) {
+    if (!IsValidTile(u, v)) {
+      return false;
+    }
+
+    tiles.erase(UDIMIndex(u, v));
+
+    return true;
+  }
+
+};
 
 #ifdef TINYUSDZ_ANDROID_LOAD_FROM_ASSETS
 extern AAssetManager *asset_manager;
@@ -64,6 +136,7 @@ std::string JoinPath(const std::string &dir, const std::string &filename);
 bool IsAbsPath(const std::string &filepath);
 
 bool IsUDIMPath(const std::string &filepath);
+
 
 bool USDFileExists(const std::string &filepath);
 
