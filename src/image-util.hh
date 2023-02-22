e99adeb7d1b78dffeb999301a9842a892ac349dd
Syoyo Fujita <syoyo@lighttransport.com>
2023-02-23 06:07:20 +0900

    Implement srgb <-> linear conversion util code.

--- src/image-util.hh
+++ src/image-util.hh
@@ -10,9 +10,9 @@
 //
 #pragma once
 
-#include <vector>
-#include <cstdlib>
 #include <cstdint>
+#include <cstdlib>
+#include <vector>
 
 namespace tinyusdz {
 
@@ -22,8 +22,6 @@ namespace tinyusdz {
 /// @param[in] in_image Input image in linear color space. Image size =
 /// [width_byte_stride/sizeof(float), height, channel_stride]
 /// @param[in] width Width pixels
-/// @param[in] width_byte_stride Width byte stride. 0 = Use `width` *
-/// channel_stride * sizeof(float)
 /// @param[in] height Height pixels
 /// @param[in] chanels Pixel channels to apply conversion. must be less than or
 /// equal to `channel_stride`
@@ -35,8 +33,7 @@ namespace tinyusdz {
 ///
 /// @return true upon success. false when any parameter is invalid.
 bool linear_to_srgb_8bit(const std::vector<float> &in_img, size_t width,
-                         size_t width_byte_stride, size_t height,
-                         size_t channels, size_t channel_stride,
+                         size_t height, size_t channels, size_t channel_stride,
                          std::vector<uint8_t> *out_img);
 
 ///
@@ -45,8 +42,6 @@ bool linear_to_srgb_8bit(const std::vector<float> &in_img, size_t width,
 /// @param[in] in_image Input image in sRGB color space. Image size =
 /// [width_byte_stride, height, channel_stride]
 /// @param[in] width Width pixels
-/// @param[in] width_byte_stride Width byte stride. 0 = Use `width` *
-/// channel_stride
 /// @param[in] height Height pixels
 /// @param[in] chanels Pixel channels to apply conversion. must be less than or
 /// equal to `channel_stride`
@@ -58,8 +53,7 @@ bool linear_to_srgb_8bit(const std::vector<float> &in_img, size_t width,
 ///
 /// @return true upon success. false when any parameter is invalid.
 bool srgb_8bit_to_linear(const std::vector<uint8_t> &in_img, size_t width,
-                         size_t width_byte_stride, size_t height,
-                         size_t channels, size_t channel_stride,
+                         size_t height, size_t channels, size_t channel_stride,
                          std::vector<float> *out_img);
 
 ///
@@ -74,8 +68,8 @@ bool srgb_8bit_to_linear(const std::vector<uint8_t> &in_img, size_t width,
 /// @param[in] chanels Pixel channels to apply conversion. must be less than or
 /// equal to `channel_stride`
 /// @param[in] chanel_stride channel stride. For example, channels=3 and
-/// channel_stride=4 to apply inverse Rec.709 convertion to RGB channel but apply
-/// linear conversion to alpha channel for RGBA image.
+/// channel_stride=4 to apply inverse Rec.709 convertion to RGB channel but
+/// apply linear conversion to alpha channel for RGBA image.
 /// @param[out] out_image Image in linear color space. Image size is same with
 /// `in_image`
 ///
@@ -84,5 +78,57 @@ bool srgb_8bit_to_linear(const std::vector<uint8_t> &in_img, size_t width,
                          size_t width_byte_stride, size_t height,
                          size_t channels, size_t channel_stride,
                          std::vector<float> *out_img);
+
+///
+/// Resize fp32 image in linear color space.
+///
+/// @param[in] in_image Input image in linear color space.
+/// @param[in] src_width Source image width pixels
+/// @param[in] src_width_byte_stride Source image width byte stride. 0 = Use
+/// `src_width` * channels
+/// @param[in] src_height Source image height pixels
+/// @param[in] dest_width Dest image width pixels
+/// @param[in] dest_width_byte_stride Dest image width byte stride. 0 = Use
+/// `src_width` * channels
+/// @param[in] src_height Dest image height pixels
+//
+/// @param[in] chanels Pixel channels both src and dest image.
+/// @param[out] dest_img Resized image in linear color space(memory is allocated
+/// inside this function).
+///
+/// @return true upon success. false when any parameter is invalid.
+bool resize_image_f32(const std::vector<float> &src_img, size_t src_width,
+                      size_t src_width_byte_stride, size_t src_height,
+
+                      size_t dest_width, size_t dest_width_byte_stride,
+                      size_t dest_height,
+
+                      size_t channels, std::vector<float> *dest_img);
+
+///
+/// Resize uint8 image in sRGB color space.
+///
+/// @param[in] in_image Input image in sRGB color space.
+/// @param[in] src_width Source image width pixels
+/// @param[in] src_width_byte_stride Source image width byte stride. 0 = Use
+/// `src_width` * channels
+/// @param[in] src_height Source image height pixels
+/// @param[in] dest_width Dest image width pixels
+/// @param[in] dest_width_byte_stride Dest image width byte stride. 0 = Use
+/// `src_width` * channels
+/// @param[in] src_height Dest image height pixels
+//
+/// @param[in] chanels Pixel channels both src and dest image.
+/// @param[out] dest_img Resized image in sRGB color space(memory is allocated
+/// inside this function).
+///
+/// @return true upon success. false when any parameter is invalid.
+bool resize_image_u8_srgb(const std::vector<uint8_t> &src_img, size_t src_width,
+                          size_t src_width_byte_stride, size_t src_height,
+
+                          size_t dest_width, size_t dest_width_byte_stride,
+                          size_t dest_height,
+
+                          size_t channels, std::vector<uint8_t> *dest_img);
 
 }  // namespace tinyusdz
