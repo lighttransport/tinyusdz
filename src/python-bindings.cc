97673284b900fe80fea95027f22d419d487d39fc
Syoyo Fujita <syoyo@lighttransport.com>
2023-03-06 23:20:05 +0900

    Python bindings experiment.

--- src/python-bindings.cc
+++ src/python-bindings.cc
@@ -1,4 +1,5 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 #include "tinyusdz.hh"
 
@@ -13,6 +14,7 @@ static double test_api() {
   return 4.14;
 }
 
+#if 0
 // stub classes
 struct PyStage
 {
@@ -49,6 +51,7 @@ struct PyStage
 
   }
 
+#if 0
   static tinyusdz::GPrim DefinePrim(const std::string &_path, const std::string &type) {
     tinyusdz::GPrim prim;
 
@@ -60,33 +63,71 @@ struct PyStage
 
     return prim;
   }
+#endif
 
 };
+#endif
 
 PYBIND11_MODULE(ctinyusdz, m) {
 
+  using namespace tinyusdz;
+
+  m.doc() = "Python binding for TinyUSDZ.";
+
   m.def("test_api", &test_api, "Test API");
 
-  auto UsdModule = m.def_submodule("Usd");
+  //auto UsdModule = m.def_submodule("Usd");
 
-  py::class_<PyStage>(UsdModule, "Stage")
+  py::class_<USDLoadOptions>(m, "USDLoadOptions")
     .def(py::init<>())
-    .def_static("Open", &PyStage::Open)
-    .def("Export", &PyStage::Export)
-    .def("GetPrimAtPath", [](const PyStage &s, const std::string &path) -> py::object {
+    .def_readwrite("num_threads", &USDLoadOptions::num_threads)
+    .def_readwrite("load_assets", &USDLoadOptions::load_assets)
+    .def_readwrite("max_memory_limit_in_mb", &USDLoadOptions::max_memory_limit_in_mb)
+    .def_readwrite("do_composition", &USDLoadOptions::do_composition)
+    ;
+
+  py::class_<Prim>(m, "Prim")
+    // default ctor: Create Prim with Model type.
+    .def(py::init([]() { return Prim(Model()); }))
+    .def_property("prim_id", [](const Prim &p) -> int64_t {
+      return p.prim_id();
+    }, [](Prim &p) -> int64_t & {
+      return p.prim_id();
+    })
+    //.def_property("children", [](const Prim &p) -> const std::vector<Prim> & {
+    //  return p.children();   
+    //}, [](Prim &p, const Prim &c) {
+    //  p.children().push_back(c);
+    //}, py::return_value_policy::reference)
+    .def("children", [](Prim &p) -> std::vector<Prim> & {
+      return p.children(); 
+    }, py::return_value_policy::reference)
+    ;
+
+
+  py::class_<Stage>(m, "Stage")
+    .def(py::init<>())
+    .def("GetPrimAtPath", [](const Stage &s, const std::string &path_str) -> py::object {
+
+      Path path(path_str, "");
+
       if (auto p = s.GetPrimAtPath(path)) {
         return py::cast(*p);
       }
+
       return py::none();
+
     });
   ;
 
-  py::class_<tinyusdz::GPrim>(UsdModule, "GPrim");
+  m.def("LoadUSDFromFile", &LoadUSDAFromFile);
+
+  //py::class_<tinyusdz::GPrim>(UsdModule, "GPrim");
 
 
-  py::class_<tinyusdz::GeomSphere>(m, "Sphere")
-    .def(py::init<>())
-  ;
+  //py::class_<tinyusdz::GeomSphere>(m, "Sphere")
+  //  .def(py::init<>())
+  //;
 
   //py::class_<tinyusdz::GeomSphere>(m, "Sphere")
   //  .def(py::init<>())
