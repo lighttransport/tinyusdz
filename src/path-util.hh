9685b91312e1eaa8fcc9decc0577f45186be45a6
Syoyo Fujita <syoyo@lighttransport.com>
2023-03-16 22:01:35 +0900

    Implement some of C API.

--- src/path-util.hh
+++ src/path-util.hh
@@ -18,6 +18,21 @@ bool ValidatePath(const Path &path, std::string *err);
 ///
 Path ConcatPath(const Path &parent, const Path &child);
 
+///
+/// Replace '../' and produce absolute path
+///
+/// base_prim_path: /root/xform
+///
+/// ../bora => /root/bora
+/// ../../bora => /bora
+/// ../../../bora => NG(return false)
+///
+/// TODO: `../` in the middle of relative path(e.g. `/root/../bora`
+///
+/// @return true upon success to resolve relative path.
+/// @return false when input paths are invalid or cannot resolve relative path.
+///
+bool ResolveRelativePath(const Path &base_prim_path, const Path &relative_path, Path *abs_path);
 
 ///
 /// Currently ToUnixishPath converts backslash character to forward slash character.
@@ -26,6 +41,7 @@ Path ConcatPath(const Path &parent, const Path &child);
 /// C:\\Users\\tinyusdz => C:/Users/tinyusdz
 ///
 Path ToUnixishPath(const Path &path);
+
 
 } // namespace path
 } // namespace tinyusdz
