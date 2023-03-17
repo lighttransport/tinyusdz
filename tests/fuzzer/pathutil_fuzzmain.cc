36670c22ee33052cae966ddd403f3724b8f200c5
Syoyo Fujita <syoyo@lighttransport.com>
2023-03-17 20:03:01 +0900

    Implement pathutil::ResolveRelativePath fuzz test.

--- tests/fuzzer/pathutil_fuzzmain.cc
+++ tests/fuzzer/pathutil_fuzzmain.cc
@@ -2,6 +2,7 @@
 #include <string>
 
 #include "tinyusdz.hh"
+#include "path-util.hh"
 
 static void run(const uint8_t *data, size_t size)
 {
@@ -30,6 +31,14 @@ static void run(const uint8_t *data, size_t size)
 
   std::string s1 = std::string(reinterpret_cast<const char *>(data), loc-1);
   std::string s2 = std::string(reinterpret_cast<const char *>(data+loc), size-loc-1);
+
+  tinyusdz::Path base_path(s1, "");
+  tinyusdz::Path rel_path(s2, "");
+  tinyusdz::Path abs_path("", "");
+
+  bool ret = tinyusdz::pathutil::ResolveRelativePath(base_path, rel_path, &abs_path);
+  (void)ret;
+
   
   return;
 }
