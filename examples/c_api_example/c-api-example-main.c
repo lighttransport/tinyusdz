f5c0f614bfe3fdeaad1ecf9525c6207e727c4360
Syoyo Fujita <syoyo@lighttransport.com>
2023-03-31 21:03:42 +0900

    Refactor C API.

--- examples/c_api_example/c-api-example-main.c
+++ examples/c_api_example/c-api-example-main.c
@@ -19,132 +19,174 @@ int prim_traverse_fun(const CTinyUSDPrim *prim, const CTinyUSDPath *path) {
 }
 
 int main(int argc, char **argv) {
-  if (argc < 2) {
-    printf("Need input.usd/usda/usdc/usdz\n");
-    return EXIT_FAILURE;
-  }
 
-  int ret = c_tinyusd_is_usd_file(argv[1]);
-  if (!ret) {
-    printf("%s is not found or not a valid USD file.\n", argv[1]);
-    return EXIT_FAILURE;
-  }
+  if (argc > 1) {
 
-  CTinyUSDStage *stage = c_tinyusd_stage_new();
-
-  c_tinyusd_string_t *warn = c_tinyusd_string_new_empty();
-  c_tinyusd_string_t *err = c_tinyusd_string_new_empty();
-
-  ret = c_tinyusd_load_usd_from_file(argv[1], stage, warn, err);
-
-  if (c_tinyusd_string_size(warn)) {
-    printf("WARN: %s\n", c_tinyusd_string_str(warn));
-  }
-
-  if (!ret) {
-    if (c_tinyusd_string_size(err)) {
-      printf("ERR: %s\n", c_tinyusd_string_str(err));
-    }
-  }
-
-  // print USD(Stage) content as ASCII.
-  c_tinyusd_string_t *str = c_tinyusd_string_new_empty();
-
-  if (!c_tinyusd_stage_to_string(stage, str)) {
-    printf("Unexpected error when exporting Stage to string.\n");
-    return EXIT_FAILURE;
-  }
-
-  printf("%s\n", c_tinyusd_string_str(str));
-
-  printf("-- traverse Prim --\n");
-  //
-  // Traverse Prims in the Stage.
-  //
-  if (!c_tinyusd_stage_traverse(stage, prim_traverse_fun, err)) {
-    if (c_tinyusd_string_size(err)) {
-      printf("Traverse error: %s\n", c_tinyusd_string_str(err));
-    }
-  }
-  printf("-- end traverse Prim --\n");
-
-  //
-  // Create new Prim
-  //
-  // You can also create a Prim with string
-  // CTinyUSDPrim *prim = c_tinyusd_prim_new("Xform");
-  CTinyUSDPrim *prim = c_tinyusd_prim_new_builtin(C_TINYUSD_PRIM_XFORM);
-
-  if (!prim) {
-    printf("Failed to new Xform Prim.\n");
-    return EXIT_FAILURE;
-  }
-
-  {
-    c_tinyusd_token_vector_t *tokv = c_tinyusd_token_vector_new_empty();
-    if (!tokv) {
-      printf("New token vector failed.\n");
+    int ret = c_tinyusd_is_usd_file(argv[1]);
+    if (!ret) {
+      printf("%s is not found or not a valid USD file.\n", argv[1]);
       return EXIT_FAILURE;
     }
 
-    if (!c_tinyusd_prim_get_property_names(prim, tokv)) {
-      printf("Failed to get property names from a Prim.\n");
+    CTinyUSDStage *stage = c_tinyusd_stage_new();
+
+    c_tinyusd_string_t *warn = c_tinyusd_string_new_empty();
+    c_tinyusd_string_t *err = c_tinyusd_string_new_empty();
+
+    ret = c_tinyusd_load_usd_from_file(argv[1], stage, warn, err);
+
+    if (c_tinyusd_string_size(warn)) {
+      printf("WARN: %s\n", c_tinyusd_string_str(warn));
+    }
+
+    if (!ret) {
+      if (c_tinyusd_string_size(err)) {
+        printf("ERR: %s\n", c_tinyusd_string_str(err));
+      }
+    }
+
+    // print USD(Stage) content as ASCII.
+    c_tinyusd_string_t *str = c_tinyusd_string_new_empty();
+
+    if (!c_tinyusd_stage_to_string(stage, str)) {
+      printf("Unexpected error when exporting Stage to string.\n");
       return EXIT_FAILURE;
     }
 
-    if (!c_tinyusd_token_vector_free(tokv)) {
-      printf("Freeing token vector failed.\n");
+    printf("%s\n", c_tinyusd_string_str(str));
+
+    printf("-- traverse Prim --\n");
+    //
+    // Traverse Prims in the Stage.
+    //
+    if (!c_tinyusd_stage_traverse(stage, prim_traverse_fun, err)) {
+      if (c_tinyusd_string_size(err)) {
+        printf("Traverse error: %s\n", c_tinyusd_string_str(err));
+      }
+    }
+    printf("-- end traverse Prim --\n");
+
+    //
+    // Release resources.
+    //
+
+    if (!c_tinyusd_string_free(str)) {
+      printf("str string free failed.\n");
       return EXIT_FAILURE;
     }
-  }
 
-  {
-    CTinyUSDAttributeValue attr_value = {0};
-    if (!c_tinyusd_attribute_value_new_int(&attr_value, 7)) {
-      printf("Failed to new `int` attribute value.\n");
+    if (!c_tinyusd_stage_free(stage)) {
+      printf("Stage free failed.\n");
+      return EXIT_FAILURE;
+    }
+    if (!c_tinyusd_string_free(warn)) {
+      printf("warn string free failed.\n");
+      return EXIT_FAILURE;
+    }
+    if (!c_tinyusd_string_free(err)) {
+      printf("err string free failed.\n");
       return EXIT_FAILURE;
     }
 
-    if (!c_tinyusd_attribute_value_to_string(&attr_value, str)) {
-      printf("Failed to print `int` attribute value.\n");
+  } else {
+
+    c_tinyusd_string_t *str = c_tinyusd_string_new_empty();
+
+    //
+    // Create new Prim
+    //
+    // You can also create a Prim with string
+    // CTinyUSDPrim *prim = c_tinyusd_prim_new("Xform");
+    CTinyUSDPrim *prim = c_tinyusd_prim_new_builtin(C_TINYUSD_PRIM_XFORM);
+
+    if (!prim) {
+      printf("Failed to new Xform Prim.\n");
       return EXIT_FAILURE;
     }
 
-    printf("Int attribute value: %s\n", c_tinyusd_string_str(str));
+    {
+      c_tinyusd_token_vector_t *tokv = c_tinyusd_token_vector_new_empty();
+      if (!tokv) {
+        printf("New token vector failed.\n");
+        return EXIT_FAILURE;
+      }
 
-    if (!c_tinyusd_attribute_value_free(&attr_value)) {
-      printf("AttributeValue free failed.\n");
-      return EXIT_FAILURE;
+      if (!c_tinyusd_prim_get_property_names(prim, tokv)) {
+        printf("Failed to get property names from a Prim.\n");
+        return EXIT_FAILURE;
+      }
+
+      if (!c_tinyusd_token_vector_free(tokv)) {
+        printf("Freeing token vector failed.\n");
+        return EXIT_FAILURE;
+      }
+    }
+
+    {
+      CTinyUSDValue *attr_value = c_tinyusd_value_new_int(7);
+      if (!attr_value) {
+        printf("Failed to new `int` value.\n");
+        return EXIT_FAILURE;
+      }
+
+      if (!c_tinyusd_value_to_string(attr_value, str)) {
+        printf("Failed to print `int` value.\n");
+        return EXIT_FAILURE;
+      }
+
+      printf("Int attribute value: %s\n", c_tinyusd_string_str(str));
+
+      if (!c_tinyusd_value_free(attr_value)) {
+        printf("Value free failed.\n");
+        return EXIT_FAILURE;
+
+      }
+    }
+
+    {
+      c_tinyusd_string_t *strval = c_tinyusd_string_new("myval");
       
+      /*
+       * NOTE: `token` and `string` value are coped, so need to free it after using it.
+       */
+      CTinyUSDValue *attr_value = c_tinyusd_value_new_string(strval);
+      if (!attr_value) {
+        printf("Failed to new `string` value.\n");
+        return EXIT_FAILURE;
+      }
+
+      if (!c_tinyusd_string_free(strval)) {
+        printf("str free failed.\n");
+        return EXIT_FAILURE;
+
+      }
+
+      if (!c_tinyusd_value_to_string(attr_value, str)) {
+        printf("Failed to print `string` value.\n");
+        return EXIT_FAILURE;
+      }
+
+      printf("String attribute value: %s\n", c_tinyusd_string_str(str));
+
+      if (!c_tinyusd_value_free(attr_value)) {
+        printf("Value free failed.\n");
+        return EXIT_FAILURE;
+
+      }
     }
+
+    if (!c_tinyusd_prim_free(prim)) {
+      printf("Prim free failed.\n");
+      return EXIT_FAILURE;
+    }
+
+    if (!c_tinyusd_string_free(str)) {
+      printf("str string free failed.\n");
+      return EXIT_FAILURE;
+    }
+
   }
 
-  //
-  // Release resources.
-  //
-
-  if (!c_tinyusd_string_free(str)) {
-    printf("str string free failed.\n");
-    return EXIT_FAILURE;
-  }
-
-  if (!c_tinyusd_prim_free(prim)) {
-    printf("Prim free failed.\n");
-    return EXIT_FAILURE;
-  }
-
-  if (!c_tinyusd_stage_free(stage)) {
-    printf("Stage free failed.\n");
-    return EXIT_FAILURE;
-  }
-  if (!c_tinyusd_string_free(warn)) {
-    printf("warn string free failed.\n");
-    return EXIT_FAILURE;
-  }
-  if (!c_tinyusd_string_free(err)) {
-    printf("err string free failed.\n");
-    return EXIT_FAILURE;
-  }
 
   return EXIT_SUCCESS;
 }
