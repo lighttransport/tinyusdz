f66c4c8383b5624ecb930cdfca88a3f48991468f
Syoyo Fujita <syoyo@lighttransport.com>
2023-04-03 23:16:17 +0900

    Python binding experiment using ctypes + C API.

--- sandbox/python/ctinyusd_test.py
+++ sandbox/python/ctinyusd_test.py
@@ -3,13 +3,13 @@ import ctypes
 import ctinyusd
 
 class Token(object):
+
     def __init__(self, tok):
-        self._handle = ctinyusd.c_tinyusd_token_init()
-        ret = ctinyusd.c_tinyusd_token_new(self._handle, tok)
-        print("init", ret)
+        self._handle = ctinyusd.c_tinyusd_token_new(tok)
+        print("init", self._handle)
 
     def __copy__(self):
-        print("copy")
+        print("copy(duplicate instance)")
         return Token(str(self))
 
     def __deepcopy__(self, memo):
@@ -25,40 +25,99 @@ class Token(object):
         print(btok)
         return btok.decode()
 
-
 class Prim(object):
-    def __init__(self):
-        self._handle = ctinyusd.CTinyUSDPrim()
-        # FIXME
-        ret = ctinyusd.c_tinyusd_prim_new("Xform", self._handle) 
-        print("init", ret)
+    
+    _builtin_types:str = [
+        "Model", # Generic Prim type
+        "Xform",
+        "Mesh",
+        # TODO..
+        ]
+    
+    # TODO: better init construction
+    def __init__(self, prim_type:str = "Model", from_handle = None):
+
+
+        if from_handle is not None:
+            # Create a Python Prim instance with handle(No copies in C layer)
+
+            print("Create Prim from handle.")
+            self._handle = from_handle 
+            self._prim_type = ctinyusd.c_tinyusd_prim_type(self._handle)
+            self._is_handle_reference = True
+
+        else:
+            # New Prim with prim_type
+            if prim_type not in self._builtin_types:
+                raise RuntimeError("Unsupported/unimplemented Prim type: ", prim_type)
+
+            self._prim_type = prim_type
+            self._handle = ctinyusd.c_tinyusd_prim_new(prim_type) 
+            self._is_handle_reference = False
+            print("init", self._handle)
 
     def __copy__(self):
-        print("copy")
-        return Prim()
+        raise RuntimeError("Copying Prim in Python side is not supported at the moment.")
 
     def __deepcopy__(self, memo):
-        print("deepcopy", memo)
-        return self.__copy__()
+        print("deep copy")
+        raise RuntimeError("Deep copying Prim in Python side is not supported at the moment.")
 
     def __del__(self):
-        ret = ctinyusd.c_tinyusd_prim_free(self._handle)
-        print("del", ret)
+        if not self._is_handle_reference:
+            ret = ctinyusd.c_tinyusd_prim_free(self._handle)
+            print("del", ret)
 
-t = Token("mytok")
-print(t)
+    def __getattr__(self, name):
+        if name == 'prim_type':
+            return self._prim_type   
+        else:
+            raise RuntimeError("Unknown Python attribute name:", name)
+            
+    def children(self):
+        return PrimChildIterator(self._handle)
 
-dt = copy.copy(t)
-del t
-print(dt)
+class PrimChildIterator:
+    def __init__(self, handle):
+        self._handle = handle
+        self._num_children = ctinyusd.c_tinyusd_prim_num_children(self._handle)
+        self._current_index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._current_index < self._num_children:
+            # Just a reference.
+            child_handle = ctinyusd.c_tinyusd_prim_get_child(self._handle, self._current_index)
+            child_prim = Prim(from_handle = child_handle)
+
+            self._current_index += 1
+
+            return child_prim
+
+        raise StopIterator
 
 
-p = Prim()
 
-a = copy.deepcopy(p)
+#t = Token("mytok")
+#print(t)
 
-print("del a")
-del a
+#dt = copy.copy(t)
+#del t
+#print(dt)
 
-print("del p")
-del p
+p = Prim("Xform")
+print(p.prim_type)
+
+
+cp = Prim(from_handle=p._handle)
+print(cp)
+
+#a = copy.deepcopy(p)
+
+children = p.children()
+print(children)
+
+#print("del p")
+#del p
