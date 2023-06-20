e890000e5e98f5171b65c14030a4b70d77612677
Syoyo Fujita <syoyo@lighttransport.com>
2023-06-21 04:36:35 +0900

    Refactor usdShade Prim struct. Add has/get/set method to sdrMetadata(usdShade).

--- src/usdShade.cc
+++ src/usdShade.cc
@@ -13,6 +13,66 @@
 
 namespace tinyusdz {
 
+bool UsdShadePrim::has_sdr_metadata(const std::string &key) {
+  if (!metas().sdrMetadata.has_value()) {
+    return false;
+  }
+
+  const CustomDataType &dict = metas().sdrMetadata.value();
+
+  if (!HasCustomDataKey(dict, key)) {
+    return false;
+  }
+
+  // check the type of value.
+  MetaVariable value;
+  if (!GetCustomDataByKey(dict, key, &value)) {
+    return false;
+  }
+
+  if (value.type_id() != value::TypeTraits<std::string>::type_id()) {
+    return false;
+  }
+
+  return true;
+}
+
+const std::string UsdShadePrim::get_sdr_metadata(const std::string &key) {
+  if (!metas().sdrMetadata.has_value()) {
+    return std::string();
+  }
+
+  const CustomDataType &dict = metas().sdrMetadata.value();
+
+  if (!HasCustomDataKey(dict, key)) {
+    return std::string();
+  }
+
+  // check the type of value.
+  MetaVariable var;
+  if (!GetCustomDataByKey(dict, key, &var)) {
+    return std::string();
+  }
+
+  if (var.type_id() != value::TypeTraits<std::string>::type_id()) {
+    return std::string();
+  }
+
+  std::string svalue;
+  if (!var.get_value(&svalue)) {
+    return std::string();
+  }
+
+  return svalue;
+}
+
+bool UsdShadePrim::set_sdr_metadata(const std::string &key, const std::string &value) {
+
+  CustomDataType &dict = metas().sdrMetadata.value();
+  bool ret = SetCustomDataByKey(key, value, dict);
+  return ret;
+}
+
 namespace {
 
 //constexpr auto kPrimvarPrefix = "primvar::";
