879bd94c3ade991948620c8678d8379686aaac53
Syoyo Fujita <syoyo@lighttransport.com>
2023-06-26 02:42:17 +0900

    [usda-parser] Add option to turn on/off composition features to use

--- examples/usda_parser/parser-main.cc
+++ examples/usda_parser/parser-main.cc
@@ -1,29 +1,79 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
+#include "io-util.hh"
+#include "str-util.hh"
 #include "stream-reader.hh"
 #include "usda-reader.hh"
-#include "io-util.hh"
 
+struct CompositionFeatures {
+  bool subLayers{true};
+  bool inherits{true};
+  bool variantSets{true};
+  bool references{true};
+  bool payloads{true};
+  bool specializes{true};
+};
 
 int main(int argc, char **argv) {
   if (argc < 2) {
-    std::cout << "usdaparser [--flatten] input.usda\n";
-    std::cout << "  --flatten: Similar to --flatten in usdview from pxrUSD.\n";
+    std::cout << "usdaparser [--flatten] [--composition=str] input.usda\n";
+    std::cout << "  --flatten: (Not implemented yet) Similar to --flatten in "
+                 "usdview from pxrUSD.\n";
+    std::cout << "  --composition: Specify which composition feature to be "
+                 "enabled. Comma separated list. \n    l "
+                 "`subLayers`, i `inherits`, v `variantSets`, r `references`, "
+                 "p `payloads`, s `specializes`. \n    Example: "
+                 "--composition=r,p --composition=references,subLayers\n";
     exit(-1);
   }
 
   std::string filename;
   // = argv[1];
 
-  bool do_compose =false;
-  size_t input_idx = 0; // 0 = invalid
+  bool do_compose = false;
+  size_t input_idx = 0;  // 0 = invalid
+  CompositionFeatures comp_features;
 
   for (size_t i = 1; i < argc; i++) {
     std::string arg = argv[i];
 
     if (arg == "--flatten") {
       do_compose = true;
+    } else if (tinyusdz::startsWith(arg, "--composition=")) {
+      std::string value_str = tinyusdz::removePrefix(arg, "--composition=");
+      if (value_str.empty()) {
+        std::cerr << "No values specified to --composition.\n";
+        exit(-1);
+      }
+
+      std::vector<std::string> items = tinyusdz::split(value_str, ",");
+      comp_features.subLayers = false;
+      comp_features.inherits = false;
+      comp_features.variantSets = false;
+      comp_features.references = false;
+      comp_features.payloads = false;
+      comp_features.specializes = false;
+
+      for (const auto &item : items) {
+        if ((item == "l") || (item == "subLayers")) {
+          comp_features.subLayers = true;
+        } else if ((item == "i") || (item == "inherits")) {
+          comp_features.inherits = true;
+        } else if ((item == "v") || (item == "variantSets")) {
+          comp_features.variantSets = true;
+        } else if ((item == "r") || (item == "references")) {
+          comp_features.references = true;
+        } else if ((item == "p") || (item == "payloads")) {
+          comp_features.payloads = true;
+        } else if ((item == "s") || (item == "specializes")) {
+          comp_features.specializes = true;
+        } else {
+          std::cerr << "Invalid string for --composition : " << item << "\n";
+          exit(-1);
+        }
+      }
+
     } else if (input_idx == 0) {
       input_idx = i;
     }
@@ -51,7 +101,8 @@ int main(int argc, char **argv) {
 
   std::vector<uint8_t> data;
   std::string err;
-  if (!tinyusdz::io::ReadWholeFile(&data, &err, filename, /* filesize_max */0)) {
+  if (!tinyusdz::io::ReadWholeFile(&data, &err, filename,
+                                   /* filesize_max */ 0)) {
     std::cerr << "Failed to open file: " << filename << ":" << err << "\n";
   }
 
@@ -65,9 +116,18 @@ int main(int argc, char **argv) {
 
   uint32_t load_states = static_cast<uint32_t>(tinyusdz::LoadState::Toplevel);
   if (do_compose) {
-    load_states = static_cast<uint32_t>(tinyusdz::LoadState::Sublayer) | 
-      static_cast<uint32_t>(tinyusdz::LoadState::Payload) | static_cast<uint32_t>(tinyusdz::LoadState::Reference);
-    std::cout << "Enable composition.\n"; 
+    if (comp_features.subLayers) {
+      load_states |= static_cast<uint32_t>(tinyusdz::LoadState::Sublayer);
+      std::cout << "Enable subLayers.\n";
+    }
+    if (comp_features.payloads) {
+      load_states |= static_cast<uint32_t>(tinyusdz::LoadState::Payload);
+      std::cout << "Enable payloads.\n";
+    }
+    if (comp_features.references) {
+      load_states |= static_cast<uint32_t>(tinyusdz::LoadState::Reference);
+      std::cout << "Enable references.\n";
+    }
   }
 
   {
