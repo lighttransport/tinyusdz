245c900fe318ff4b9a8e503f7c83e276d9bfd70f
Syoyo Fujita <syoyo@lighttransport.com>
2023-08-04 05:31:09 +0900

    [USDA printer] Fix `.connect` was missing in Material's output attributes.

--- src/pprinter.cc
+++ src/pprinter.cc
@@ -2725,8 +2725,9 @@ std::string to_string(const Material &material, const uint32_t indent, bool clos
 
   if (material.surface.authored()) {
 
+    // assume connection when authored.
     // TODO: list edit?.
-    ss << pprint::Indent(indent+1) << "token outputs:surface ";
+    ss << pprint::Indent(indent+1) << "token outputs:surface.connect ";
 
     const auto &conns = material.surface.get_connections();
     if (conns.size() == 1) {
@@ -2749,8 +2750,9 @@ std::string to_string(const Material &material, const uint32_t indent, bool clos
   }
 
   if (material.displacement.authored()) {
+    // assume connection when authored.
     // TODO: list edit?.
-    ss << pprint::Indent(indent+1) << "token outputs:displacement ";
+    ss << pprint::Indent(indent+1) << "token outputs:displacement.connect ";
 
     const auto &conns = material.displacement.get_connections();
     if (conns.size() == 1) {
@@ -2773,8 +2775,9 @@ std::string to_string(const Material &material, const uint32_t indent, bool clos
   }
 
   if (material.volume.authored()) {
+    // assume connection when authored.
     // TODO: list edit?.
-    ss << pprint::Indent(indent+1) << "token outputs:volume ";
+    ss << pprint::Indent(indent+1) << "token outputs:volume.connect ";
 
     const auto &conns = material.volume.get_connections();
     if (conns.size() == 1) {
