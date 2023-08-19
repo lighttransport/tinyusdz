78cf3e9c2d6d104b598e25a1bdca92b06881506b
Syoyo Fujita <syoyo@lighttransport.com>
2023-08-20 06:01:43 +0900

    [W.I.P] parsing a value of materialx type.

--- src/usdMtlx.cc
+++ src/usdMtlx.cc
@@ -14,6 +14,7 @@
 #include <sstream>
 
 #include "io-util.hh"
+#include "tiny-format.hh"
 #include "ascii-parser.hh" // To parse color3f value
 #include "common-macros.inc"
 #include "usdMtlx.hh"
@@ -24,9 +25,157 @@
   } \
 } while(0);
 
+
 namespace tinyusdz {
 
+// defined in ascii-parser-base-types.cc
+namespace ascii {
+
+extern template bool AsciiParser::SepBy1BasicType<float>(const char sep, std::vector<float> *ret);
+
+} // namespace ascii
+
 namespace detail {
+
+bool is_supported_type(const std::string &typeName);
+
+bool is_supported_type(const std::string &typeName) {
+
+  if (typeName.compare("integer") == 0) return true;
+  if (typeName.compare("boolean") == 0) return true;
+  if (typeName.compare("float") == 0) return true;
+  if (typeName.compare("color3") == 0) return true;
+  if (typeName.compare("color4") == 0) return true;
+  if (typeName.compare("vector2") == 0) return true;
+  if (typeName.compare("vector3") == 0) return true;
+  if (typeName.compare("vector4") == 0) return true;
+  if (typeName.compare("matrix33") == 0) return true;
+  if (typeName.compare("matrix44") == 0) return true;
+  if (typeName.compare("string") == 0) return true;
+  if (typeName.compare("filename") == 0) return true;
+
+  if (typeName.compare("integerarray") == 0) return true;
+  if (typeName.compare("floatarray") == 0) return true;
+  if (typeName.compare("vector2array") == 0) return true;
+  if (typeName.compare("vector3array") == 0) return true;
+  if (typeName.compare("vector4array") == 0) return true;
+  if (typeName.compare("color3array") == 0) return true;
+  if (typeName.compare("color4array") == 0) return true;
+  if (typeName.compare("stringarray") == 0) return true;
+
+  // No matrixarray
+
+  // TODO
+  //if (typeName.compare("color") == 0) return true;
+  //if (typeName.compare("geomname") == 0) return true;
+  //if (typeName.compare("geomnamearray") == 0) return true;
+  
+  return false;
+}
+
+template<typename T>
+bool ParseValue(tinyusdz::ascii::AsciiParser &parser, T &ret, std::string *err);
+
+template<>
+bool ParseValue<int>(tinyusdz::ascii::AsciiParser &parser, int &ret, std::string *err) {
+
+  int val;
+  if (!parser.ReadBasicType(&val)) {
+    PUSH_ERROR_AND_RETURN(fmt::format("Failed to parse a value of type `{}`", value::TypeTraits<int>::type_name()));
+  }
+
+  ret = val;
+
+  return true;
+}
+
+template<>
+bool ParseValue<bool>(tinyusdz::ascii::AsciiParser &parser, bool &ret, std::string *err) {
+
+  bool val;
+  if (!parser.ReadBasicType(&val)) {
+    PUSH_ERROR_AND_RETURN(fmt::format("Failed to parse a value of type `{}`", value::TypeTraits<bool>::type_name()));
+  }
+
+  ret = val;
+
+  return true;
+}
+
+template<>
+bool ParseValue<float>(tinyusdz::ascii::AsciiParser &parser, float &ret, std::string *err) {
+
+  float val;
+  if (!parser.ReadBasicType(&val)) {
+    PUSH_ERROR_AND_RETURN(fmt::format("Failed to parse a value of type `{}`", value::TypeTraits<float>::type_name()));
+  }
+
+  ret = val;
+
+  return true;
+}
+
+template<>
+bool ParseValue<value::float2>(tinyusdz::ascii::AsciiParser &parser, value::float2 &ret, std::string *err) {
+
+
+  std::vector<float> values;
+  if (!parser.SepBy1BasicType(',', &values)) {
+    PUSH_ERROR_AND_RETURN(fmt::format("Failed to parse a value of type `{}`", value::TypeTraits<value::float3>::type_name()));
+  }
+
+  if (values.size() != 2) {
+    PUSH_ERROR_AND_RETURN(fmt::format("type `{}` expects the number of elements is 2, but got {}",
+       value::TypeTraits<value::float2>::type_name(), values.size()));
+  }
+
+  ret[0] = values[0];
+  ret[1] = values[1];
+
+  return true;
+}
+
+template<>
+bool ParseValue<value::float3>(tinyusdz::ascii::AsciiParser &parser, value::float3 &ret, std::string *err) {
+
+
+  std::vector<float> values;
+  if (!parser.SepBy1BasicType(',', &values)) {
+    PUSH_ERROR_AND_RETURN(fmt::format("Failed to parse a value of type `{}`", value::TypeTraits<value::float3>::type_name()));
+  }
+
+  if (values.size() != 3) {
+    PUSH_ERROR_AND_RETURN(fmt::format("type `{}` expects the number of elements is 3, but got {}",
+       value::TypeTraits<value::float3>::type_name(), values.size()));
+  }
+
+  ret[0] = values[0];
+  ret[1] = values[1];
+  ret[2] = values[2];
+
+  return true;
+}
+
+template<>
+bool ParseValue<value::float4>(tinyusdz::ascii::AsciiParser &parser, value::float4 &ret, std::string *err) {
+
+  std::vector<float> values;
+  if (!parser.SepBy1BasicType(',', &values)) {
+    PUSH_ERROR_AND_RETURN(fmt::format("Failed to parse a value of type `{}`", value::TypeTraits<value::float4>::type_name()));
+  }
+
+  if (values.size() != 4) {
+    PUSH_ERROR_AND_RETURN(fmt::format("type `{}` expects the number of elements is 4, but got {}",
+       value::TypeTraits<value::float4>::type_name(), values.size()));
+  }
+
+  ret[0] = values[0];
+  ret[1] = values[1];
+  ret[2] = values[2];
+  ret[3] = values[3];
+
+  return true;
+}
 
 ///
 /// For MaterialX XML.
@@ -47,6 +196,52 @@ namespace detail {
 ///
 bool ParseMaterialXValue(const std::string &typeName, const std::string &str,
                            value::Value *value, std::string *err);
+
+
+bool ParseMaterialXValue(const std::string &typeName, const std::string &str,
+                           value::Value *value, std::string *err) {
+
+  (void)value;
+
+  if (!is_supported_type(typeName)) {
+    PUSH_ERROR_AND_RETURN(fmt::format("Invalid/unsupported type: {}", typeName));
+  }
+
+  tinyusdz::StreamReader sr(reinterpret_cast<const uint8_t *>(str.data()), str.size(), /* swap endian */ false);
+  tinyusdz::ascii::AsciiParser parser(&sr);
+
+  if (typeName.compare("integer") == 0) {
+    int val;
+    if (!ParseValue(parser, val, err)) {
+      return false;
+    }
+  } else if (typeName.compare("boolean") == 0) {
+    bool val;
+    if (!ParseValue(parser, val, err)) {
+      return false;
+    }
+  } else if (typeName.compare("vector2") == 0) {
+    value::float2 val;
+    if (!ParseValue(parser, val, err)) {
+      return false;
+    }
+  } else if (typeName.compare("vector3") == 0) {
+    value::float3 val;
+    if (!ParseValue(parser, val, err)) {
+      return false;
+    }
+  } else if (typeName.compare("vector4") == 0) {
+    value::float4 val;
+    if (!ParseValue(parser, val, err)) {
+      return false;
+    }
+  } else {
+    PUSH_ERROR_AND_RETURN("TODO: " + typeName);
+  }
+
+  // TODO
+  return false;
+}
 
 
 } // namespace detail
