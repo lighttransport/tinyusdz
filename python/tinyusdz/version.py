534ecad7cddb64cd3a8d50482dc40d09dff220e7
Syoyo Fujita <syoyo@lighttransport.com>
2024-01-03 21:20:24 +0900

    v0.8.0rc6

--- python/tinyusdz/version.py
+++ python/tinyusdz/version.py
@@ -3,8 +3,12 @@
 major_version=0
 minor_version=8
 micro_version=0
-extra_revision="rc4"
+extra_revision="rc6"
 
 version = "{}.{}.{}".format(major_version, minor_version, micro_version)
+
 if isinstance(extra_revision, str):
     version += extra_revision
+
+version_tag = "v" + version
+
