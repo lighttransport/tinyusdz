1a4ba0436f6538f7dc53fd9409d351f7f515d8a3
Syoyo Fujita <syoyo@lighttransport.com>
2024-06-14 21:53:10 +0900

    add texcoord2f -> float2 cast test.

--- tests/unit/unit-value-types.cc
+++ tests/unit/unit-value-types.cc
@@ -7,6 +7,7 @@
 
 #include "unit-value-types.h"
 #include "value-types.hh"
+#include "math-util.inc"
 
 using namespace tinyusdz;
 
@@ -26,6 +27,17 @@ void value_types_test(void) {
   TEST_CHECK(value::GetTypeId("token[]") == (value::TYPE_ID_TOKEN|value::TYPE_ID_1D_ARRAY_BIT));
 
   TEST_CHECK(!value::TryGetTypeName(value::TYPE_ID_ALL));
+
+  // texCoord2f <-> float2 cast
+  value::float2 uv{1.0f, 2.0f};
+  value::Value value(uv);
+
+  value::texcoord2f *tex2f = value.as<value::texcoord2f>();
+  TEST_CHECK(tex2f != nullptr);
+  if (tex2f) {
+    TEST_CHECK(math::is_close(tex2f->s, 1.0f));
+    TEST_CHECK(math::is_close(tex2f->t, 2.0f));
+  }
 
 }
 
