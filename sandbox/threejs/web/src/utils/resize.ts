8aa7909340b4fd87f55839173f17164a6831a6a0
Syoyo Fujita <syoyo@lighttransport.com>
2024-08-16 01:38:00 +0900

    threejs web app.

---
+++ sandbox/threejs/web/src/utils/resize.ts
@@ -0,0 +1,12 @@
+function resizeRendererToDisplaySize(renderer: THREE.WebGLRenderer) {
+  const canvas = renderer.domElement;
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
+
+export { resizeRendererToDisplaySize };
