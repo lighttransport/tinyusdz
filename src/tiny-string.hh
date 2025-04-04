a4cac40bdd4dd5ab4ff6ca3868645f54148488c9
Syoyo Fujita <syoyo@lighttransport.com>
2025-04-04 23:34:29 +0900

    tstring w.i.p.

--- src/tiny-string.hh
+++ src/tiny-string.hh
@@ -10,6 +10,7 @@
 #include <memory>
 #include <cstdlib>
 #include <cstring>
+#include <iostream> // dbg
 
 #include "tiny-container.hh"
 
@@ -35,6 +36,7 @@ static size_t strlen(const char *s, size_t max_len = 1024u*1024u*1024u) {
 
 template<size_t N = 8>
 struct tstring_n {
+  // 8 = enough size to store pointer address
   static_assert(N >= 8, "N must be 8 or larger.");
 
  public:
@@ -86,7 +88,11 @@ struct tstring_n {
   }
 
   const char *c_str() const {
-    return reinterpret_cast<const char *>(_buf);
+    if (_len >= N) {
+      return *reinterpret_cast<const char **>(&_buf);
+    } else {
+      return reinterpret_cast<const char *>(_buf);
+    }
   }
 
   size_t size() const {
@@ -94,7 +100,12 @@ struct tstring_n {
   }
 
   std::string to_std_string() {
-    const char *p = reinterpret_cast<const char *>(_buf);
+    const char *p;
+    if (_len < N) {
+      p = reinterpret_cast<const char *>(_buf);
+    } else {
+      p = *reinterpret_cast<const char **>(&_buf);
+    }
     std::string s(p, _len);
     return s;
   }
@@ -102,8 +113,8 @@ struct tstring_n {
  private:
   void _delete_string() {
     if (_len >= N) {
-      char *p = reinterpret_cast<char *>(_buf);
-      delete[] p;
+      char **p = reinterpret_cast<char **>(&_buf);
+      delete[] (*p);
     }
     memset(_buf, 0, 8);
     _len = 0;
@@ -111,29 +122,34 @@ struct tstring_n {
 
   void _copy_string(const char *s) {
 
-    if (_len > 0) {
-      if (_len >= N) {
-        char *p = reinterpret_cast<char *>(_buf);
-        delete[] p;
-        memset(_buf, 0, 8);
-      }
-    }
+    _delete_string();
 
-    char *dst = reinterpret_cast<char *>(_buf);
+    //if (_len > 0) {
+    //  if (_len >= N) {
+    //    //char *p = reinterpret_cast<char *>(_buf);
+    //    //delete[] p;
+    //    //memset(_buf, 0, 8);
+    //  }
+    //}
+
+    //char *dst = reinterpret_cast<char *>(_buf);
 
     _len = strlen(s);    
     if (_len >= N) {
-      dst = new char[_len+1];
-    } else {
+      char *dst = new char[_len+1];
       memcpy(dst, s, _len);
+      dst[_len] = '\0';
+
+      *(reinterpret_cast<char **>(&_buf[0])) = dst;
+    } else {
+      memcpy(_buf, s, _len);
     }
-    dst[_len] = '\0';
 
   }
 
   // TODO: Ues custom vector class.
   //char *_s{nullptr}; // end with '\0'
-  char _buf[N]{};
+  char _buf[N+1]{}; // store pointer address when _len > N
   size_t _len{0};
 };
 
@@ -159,6 +175,24 @@ struct tstring_view {
   constexpr tstring_view(const tstring &s) : tstring_view(s.c_str()) { 
   }
 
+  bool operator==(const tstring_view &rhs) {
+    if (_len != rhs.size()) {
+      return false;
+    }
+
+    for (size_t i = 0; i < _len; i++) {
+      if (_s[i] != rhs.c_str()[i]) {
+        return false;
+      }
+    }   
+    
+    return true;
+  }
+
+  bool operator!=(const tstring_view &rhs) {
+    return !(*this == rhs);
+  }
+
   const char *c_str() const {
     return _s;
   }
@@ -172,10 +206,12 @@ struct tstring_view {
   {
     size_t sv_size = sv.size();
 
+    std::cout << "len " << _len << ", sv_size " << sv_size << "\n";
     if (_len < sv_size) {
       return false;
     }
     const char *sv_str = sv.c_str();
+    std::cout << "sv_str " << sv_str << "\n";
 
     for (size_t i = 0; i < sv_size; i++) {
       if (_s[i] != sv_str[i]) {
@@ -188,6 +224,8 @@ struct tstring_view {
   bool starts_with( const char *s) const {
     size_t s_size = strlen(s);
 
+    std::cout << "len " << _len << ", s_size " << s_size << "\n";
+
     if (_len < s_size) {
       return false;
     }
@@ -197,6 +235,72 @@ struct tstring_view {
       }
     }   
     return true;
+  }
+
+  bool ends_with( tstring_view sv ) const noexcept
+  {
+    size_t sv_size = sv.size();
+
+    if (_len < sv_size) {
+      return false;
+    }
+    const char *sv_str = sv.c_str();
+
+    for (size_t i = 0; i < sv_size; i++) {
+      if (_s[_len - i - 1] != sv_str[sv_size - i - 1]) {
+        return false;
+      }
+    }   
+    return true;
+  }
+
+  bool ends_with( const char *s) const {
+    size_t s_size = strlen(s);
+
+    if (_len < s_size) {
+      return false;
+    }
+    for (size_t i = 0; i < s_size; i++) {
+      if (_s[_len - i - 1] != s[s_size - i - 1]) {
+        return false;
+      }
+    }   
+    return true;
+  }
+
+  bool contains( const char *s) const {
+    size_t s_size = strlen(s);
+    if (s_size == 0) {
+      return false;
+    }
+
+    if (_len < s_size) {
+      return false;
+    }
+
+    size_t i_s{0};
+    for (size_t i = 0; i < _len; i++) {
+      if (_s[i] == s[0]) {
+        i_s = i;
+        break;
+      }
+    }
+
+    if (_len < i_s + s_size) {
+      return false; 
+    }
+
+    for (size_t i = i_s; i < i_s + s_size; i++) {
+      if (_s[i] != s[i - i_s]) {
+        return false;
+      }
+    }   
+
+    return true;
+  }
+
+  bool contains( tstring_view sv) const {
+    return contains(sv.c_str());
   }
 
  private:
