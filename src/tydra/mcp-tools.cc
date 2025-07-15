2c0ffc4ee0d189164ef85bc01da00fe547e41575
Syoyo Fujita <syoyo@lighttransport.com>
2025-07-15 11:22:18 +0900

    mcp w.i.p

--- src/tydra/mcp-tools.cc
+++ src/tydra/mcp-tools.cc
@@ -47,18 +47,20 @@ bool GetVersion(nlohmann::json &result) {
 
 
 bool LoadUSDLayerFromFile(Context &ctx, const nlohmann::json &args, nlohmann::json &result, std::string &err) {
+  DCOUT("args " << args);
   if (!args.contains("uri")) {
+    DCOUT("uri param not found");
     err = "`uri` param not found.\n";
     return false; 
   }
 
   std::string uri = args["uri"];
-
   
   Layer layer;
   std::string warn;
   USDLoadOptions options; 
   if (!LoadLayerFromFile(uri, &layer, &warn, &err, options)) {
+    DCOUT("Failed to load layer from file: " << err);
     err = "Failed to load layer from file: " + err + "\n";
     return false;
   }
@@ -70,6 +72,7 @@ bool LoadUSDLayerFromFile(Context &ctx, const nlohmann::json &args, nlohmann::js
   std::string uuid = generateUUID();
 
   if (ctx.layers.count(uuid)) {
+    DCOUT("uuid conflict");
     // This should not be happen.
     err = "Internal error. UUID conflict\n";
     return false;
@@ -81,6 +84,45 @@ bool LoadUSDLayerFromFile(Context &ctx, const nlohmann::json &args, nlohmann::js
 
   ctx.layers.emplace(uuid, std::move(usd_layer));
 
+  DCOUT("loaded USD as Layer");
+
+  nlohmann::json content;
+  content["type"] = "text";
+  content["text"] = uuid;
+
+  result["content"] = nlohmann::json::array();
+  result["content"].push_back(content);
+
+  return true;
+}
+
+bool ListPrimSpecs(Context &ctx, const nlohmann::json &args, nlohmann::json &result, std::string &err) {
+  DCOUT("args " << args);
+  if (!args.contains("uuid")) {
+    DCOUT("uuid param not found");
+    err = "`uuid` param not found.\n";
+    return false; 
+  }
+
+  std::string uuid = args["uuid"];
+
+  if (!ctx.layers.count(uuid)) {
+    DCOUT("Layer not found: " << uuid);
+    err = "Layer not found: " + uuid;
+    return false;
+  }
+
+  const USDLayer &usd_layer = ctx.layers.at(uuid);
+
+  result["content"] = nlohmann::json::array();
+
+  for (const auto &ps : usd_layer.layer.primspecs()) {
+    nlohmann::json content;
+    content["type"] = "text";
+    content["text"] = ps.first;
+    result["content"].push_back(content);
+  }
+  
   return true;
 }
 
@@ -105,6 +147,44 @@ bool GetToolsList(nlohmann::json &result) {
     result["tools"].push_back(j);
   }
 
+  {
+    nlohmann::json j;
+    j["name"] = "load_usd_layer";
+    j["description"] = "Load USD as Layer from URI(currently local file only)";
+
+    nlohmann::json schema;
+    schema["type"] = "object";
+    schema["properties"] = nlohmann::json::object();
+    schema["properties"]["uri"] ={{"type", "string"}};
+
+    schema["required"] = nlohmann::json::array({"uri"});
+
+    j["inputSchema"] = schema;
+
+    result["tools"].push_back(j);
+
+  }
+
+  {
+    nlohmann::json j;
+    j["name"] = "list_primspecs";
+    j["description"] = "List root PrimSpecs in loaded USD Layer";
+
+    nlohmann::json schema;
+    schema["type"] = "object";
+    schema["properties"] = nlohmann::json::object();
+    schema["properties"]["uuid"] ={{"type", "string"}};
+
+    schema["required"] = nlohmann::json::array({"uuid"});
+
+    j["inputSchema"] = schema;
+
+    result["tools"].push_back(j);
+
+  }
+
+  std::cout << result << "\n";
+
   return true;
 }
 
@@ -115,7 +195,11 @@ bool CallTool(Context &ctx, const std::string &tool_name, const nlohmann::json &
   if (tool_name == "get_version") {
     return GetVersion(result);
   } else if (tool_name == "load_usd_layer") {
+    DCOUT("load_usd_layer");
     return LoadUSDLayerFromFile(ctx, args, result, err);
+  } else if (tool_name == "list_primspecs") {
+    DCOUT("list_primspecs");
+    return ListPrimSpecs(ctx, args, result, err);
   }
 
   // tool not found.
