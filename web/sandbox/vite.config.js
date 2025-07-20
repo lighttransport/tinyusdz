2ad412b9ec511a7d7831554e42e2849eb3cc539c
Syoyo Fujita <syoyo@lighttransport.com>
2025-07-20 10:22:44 +0900

    mcp client w.i.p.

---
+++ web/sandbox/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+
+// Do not minify(we want to make demo website simple)
+export default defineConfig({
+    server: {
+        headers: {
+            'Cross-Origin-Opener-Policy': 'same-origin',
+            'Cross-Origin-Embedder-Policy': 'require-corp',
+        },
+    },
+    optimizeDeps: {
+        exclude: ['tinyusdz'],
+    }
+});
