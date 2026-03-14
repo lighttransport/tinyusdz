40ffad8cfd972d805d5df808f5f8ea7bc65c0470
Syoyo Fujita <syoyo@lighttransport.com>
2026-03-15 00:53:09 +0900

    Improve tydra shader scene-access

--- tests/unit/unit-tydra.h
+++ tests/unit/unit-tydra.h
@@ -5,3 +5,4 @@ void tydra_inplace_conversion_guard_test(void);
 void tydra_geommesh_property_accessor_test(void);
 void tydra_memory_tracking_test(void);
 void tydra_scene_access_helper_test(void);
+void tydra_shader_scene_access_test(void);
