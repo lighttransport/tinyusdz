643d8819cd89e05ee73789798e2a0a36403c2de4
Syoyo Fujita <syoyo@lighttransport.com>
2026-07-27 14:49:36 +0900

    Extend vendored Ptex sample formats

--- src/external/tinyexr/tools/ptexatlas/ptex.h
+++ src/external/tinyexr/tools/ptexatlas/ptex.h
@@ -5,8 +5,9 @@
 #define TINYEXR_PTEX_H_
 #include <stddef.h>
 #include <stdint.h>
-typedef struct tinyexr_ptex_info { uint16_t channels, levels; uint32_t faces; } tinyexr_ptex_info;
-typedef struct tinyexr_ptex_face { uint32_t width, height, channels; uint8_t *pixels; } tinyexr_ptex_face;
+typedef enum tinyexr_ptex_type { TINYEXR_PTEX_UINT8 = 0, TINYEXR_PTEX_UINT16 = 1, TINYEXR_PTEX_HALF = 2, TINYEXR_PTEX_FLOAT = 3 } tinyexr_ptex_type;
+typedef struct tinyexr_ptex_info { uint16_t channels, levels; uint32_t faces; tinyexr_ptex_type type; } tinyexr_ptex_info;
+typedef struct tinyexr_ptex_face { uint32_t width, height, channels, bytes_per_channel; uint8_t *pixels; } tinyexr_ptex_face;
 int tinyexr_ptex_info_memory(const uint8_t *data, size_t size, tinyexr_ptex_info *info);
 int tinyexr_ptex_read_memory(const uint8_t *data, size_t size, uint32_t face,
                              uint32_t level, size_t max_bytes,
