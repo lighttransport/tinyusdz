c2040ab22f9d01377229b5b513ee81ba583452ec
Syoyo Fujita <syoyo@lighttransport.com>
2026-07-27 19:45:52 +0900

    Sample primary Ptex semantics in OpenGL raster

--- examples/tusdview/gl/gl_renderer.hh
+++ examples/tusdview/gl/gl_renderer.hh
@@ -218,6 +218,9 @@ class GLRenderer final : public Renderer {
   GLint uKind_{-1};                                       // kind AOV (per-draw)
   GLint uFaceIdTex_{-1}, uFaceBase_{-1}, uHasFaceId_{-1}; // source-face-id AOV
   GLint uBasePtex_{-1}, uBasePtexGrid_{-1};              // Ptex atlas sampling
+  GLint uMetallicPtexGrid_{-1}, uRoughnessPtexGrid_{-1};
+  GLint uNormalPtexGrid_{-1}, uEmissivePtexGrid_{-1};
+  GLint uOpacityPtexGrid_{-1}, uOcclusionPtexGrid_{-1};
   GLint uBaseColor_{-1}, uMetallic_{-1}, uRoughness_{-1}, uEmissive_{-1}, uAlpha_{-1};
   GLint uAlphaMode_{-1}, uAlphaCutoff_{-1};
   GLint uUseSpecularWorkflow_{-1}, uOpenPbrSpecularModel_{-1};
