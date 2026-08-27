755c4ccf290c5d61a7e468f54e50ece80c3a5269
Syoyo Fujita <syoyo@lighttransport.com>
2026-08-27 16:06:44 +0900

    Preserve closure normals in MaterialX bridge

--- examples/tusdview/tests/lightrt_bridge_test.cc
+++ examples/tusdview/tests/lightrt_bridge_test.cc
@@ -2497,6 +2497,17 @@ int main() {
   if (!CheckDirectClosure(R"json({
     "nodegraph":{"nodes":[
       {"name":"diff","category":"diffuse_bsdf","type":"BSDF",
+       "inputs":[{"name":"weight","value":0.8},
+                 {"name":"color","value":[0.7,0.4,0.2]},
+                 {"name":"normal","value":[0.2,0.3,0.93]}]}
+    ],"outputs":[{"name":"shader","type":"BSDF","nodename":"diff"}]},
+    "connections":[{"input":"bsdf","output":"shader"}]})json",
+      "closure normal lane", {0, 5, 20, 21})) {
+    return 1;
+  }
+  if (!CheckDirectClosure(R"json({
+    "nodegraph":{"nodes":[
+      {"name":"diff","category":"diffuse_bsdf","type":"BSDF",
        "inputs":[{"name":"weight","value":0.6},{"name":"color","value":[0.3,0.5,0.7]}]},
       {"name":"emit","category":"uniform_edf","type":"EDF",
        "inputs":[{"name":"color","value":[0.1,0.2,0.4]}]},
